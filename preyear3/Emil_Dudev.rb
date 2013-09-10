7bb9395df79dc137251c738c015f53b8a50c49ad
edudev <emildudev@gmail.com>
2013-09-10 13:49:05 +0300

    Reply to a question

--- preyear3/Emil_Dudev.rb
+++ preyear3/Emil_Dudev.rb
@@ -5,6 +5,13 @@ f = File.open(ARGV[0]).each do |line|
 	c += 1 if line =~ /^\d\d:\d\d:\d\d,\d\d\d\s*-->\s*\d\d:\d\d:\d\d,\d\d\d\s*$/
 end
 # Is closing neccessary in Ruby and in the above code?
+# Reply: The file is still open. Simple check:
+# puts f.closed?
+# If the block was opened as File.open(ARGV[0]) do |f| ...
+# then it wouldn't be necessary to close it (actually, closing it might cause an error).
+# I can only guess that if the file is left open, Ruby will automatically close it upon exit.
+# Nevertheless I wrote the code and it's my responsibility to free the resources I've created.
+# Also, the garbage collector shouldn't touch the file while it's open.
 f.close
 puts c
 
