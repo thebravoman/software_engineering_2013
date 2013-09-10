83629c2ce25d1cf64ca532a9b62f85fd15402158
Kiko <k.mitov@robopartans.com>
2013-09-10 08:33:41 +0300

    Commenting on Emil Dubev work

--- preyear3/Emil_Dudev.rb
+++ preyear3/Emil_Dudev.rb
@@ -4,6 +4,7 @@ c = 0
 f = File.open(ARGV[0]).each do |line|
 	c += 1 if line =~ /^\d\d:\d\d:\d\d,\d\d\d\s*-->\s*\d\d:\d\d:\d\d,\d\d\d\s*$/
 end
+# Is closing neccessary in Ruby and in the above code?
 f.close
 puts c
 
