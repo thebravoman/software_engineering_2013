66f8daa8c9dac115d429a74ed39bc8ec1270540d
Kiko <k.mitov@robopartans.com>
2013-09-11 09:41:59 +0300

    Second question for Emil_Dudev

--- preyear3/Emil_Dudev.rb
+++ preyear3/Emil_Dudev.rb
@@ -4,7 +4,7 @@ c = 0
 f = File.open(ARGV[0]).each do |line|
 	c += 1 if line =~ /^\d\d:\d\d:\d\d,\d\d\d\s*-->\s*\d\d:\d\d:\d\d,\d\d\d\s*$/
 end
-# Is closing neccessary in Ruby and in the above code?
+# Q1: Is closing neccessary in Ruby and in the above code?
 # Reply: The file is still open. Simple check:
 # puts f.closed?
 # If the block was opened as File.open(ARGV[0]) do |f| ...
@@ -12,6 +12,9 @@ end
 # I can only guess that if the file is left open, Ruby will automatically close it upon exit.
 # Nevertheless I wrote the code and it's my responsibility to free the resources I've created.
 # Also, the garbage collector shouldn't touch the file while it's open.
+# 
+# Q2: Is the f reference really needed in this program. 
+# Can the program work if we remove it and along with it remove the closing?
 f.close
 puts c
 
