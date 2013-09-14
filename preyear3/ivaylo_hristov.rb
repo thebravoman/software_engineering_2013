c41d45127bda501e1aa5f6629b9b56242f679e56
ivaylohristov96 <ivaylo.hhristov96@gmail.com>
2013-09-14 03:36:08 +0300

    Task 3 - Ivaylo Hristov

---
+++ preyear3/ivaylo_hristov.rb
@@ -0,0 +1,15 @@
+if ARGV.length != 1
+	puts "Incorrect usage\nUsage: ruby ProgramName.rb SRTFile.srt"
+elsif File.exists?(ARGV[0])
+	counter = 0
+	subf = ARGV[0]
+	searcher = /-->/
+	File.readlines(subf).each do |line|
+		if line =~ searcher
+			counter += 1
+		end
+	end
+	puts "The number of lines in the SRT File: #{counter}"
+else
+	puts "Incorrect filename/Unexisting file"
+end
