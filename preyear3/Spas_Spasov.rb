e47a559601c3547d4c892a8f1b000be5d82f9af2
Spas <s.spasov96@abv.bg>
2013-09-14 18:53:06 +0300

    Create Spas_Spasov.rb

---
+++ preyear3/Spas_Spasov.rb
@@ -0,0 +1,12 @@
+count = 0
+lookfor = / --> /
+filename = ARGV[0]
+fn = File.open("#{filename}", "r")
+fn.each do |line|
+
+	if line=~ lookfor
+		count = +=1
+	end
+end
+
+puts "The number of subtitles is  : #{count}"
