d150c8de00ac47ae11980c08fef958c2a168843a
Ivaylo Markov <ivailo96@gmail.com>
2013-09-26 21:11:29 +0300

    Task 1

---
+++ class2_homework/Ivaylo_Markov_1.rb
@@ -0,0 +1,19 @@
+require 'csv'
+require 'date'
+
+
+begd = Date.parse(ARGV[0])
+endd = Date.parse(ARGV[1])
+income = 0.00
+spent = 0.00
+
+#Dir.chdir(File.expand_path(File.dirname(File.dirname(__FILE__))))
+
+CSV.foreach('bank.csv') do |line| 
+	datec = Date.parse(line[0])
+	if datec >= begd && datec <= endd
+		income = income + line[1].to_f
+		spent = spent + line[2].to_f
+	end
+end
+puts "#{income},#{spent},#{income - spent}"
