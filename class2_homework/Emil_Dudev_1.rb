a5b59b8c95921ddeee97df55e48f38edf334ca54
Emil Dudev <emildudev@gmail.com>
2013-09-26 15:45:55 +0300

    Problem 1 solution

---
+++ class2_homework/Emil_Dudev_1.rb
@@ -0,0 +1,18 @@
+#!/urb/bin/ruby
+
+require 'csv'
+require 'date'
+
+s = Date.strptime( ARGV[0], '%d/%m/%Y' )
+e = Date.strptime( ARGV[1], '%d/%m/%Y' )
+c = 0.0
+d = 0.0
+CSV.foreach('bank.csv') do |r|
+	t = Date.strptime( r[0], '%d/%m/%Y' )
+	if t >= s && t <= e
+		d += r[1].to_f
+		c += r[2].to_f
+	end
+end
+printf "%.2f,%.2f,%.2f\n", d, c, d-c
+
