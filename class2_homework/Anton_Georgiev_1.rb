4ac959c89ccc65690136f4d0e4a7c912c267f551
anton4o123 <anton4o123@abv.bg>
2013-09-28 22:01:09 +0300

    Program 1

---
+++ class2_homework/Anton_Georgiev_1.rb
@@ -0,0 +1,12 @@
+require 'csv'
+
+x=0.00
+y=0.00
+
+CSV.foreach("bank.csv") do |row|
+   if(row[0].to_i>=ARGV[0].to_i) && (row[0].to_i<=ARGV[1].to_i)
+   x=x+row[1].to_f
+   y=y+row[2].to_f
+   end
+end
+print x,",",y,",",x-y,"\n"
