3de3eb3c6f1c103dc4e2734caa7e7012c67dbadd
petur-pelovski-tues <peturpelovski@gmail.com>
2013-09-28 22:17:39 +0300

    Update Петър_Пеловски_1.rb

--- class2_homework/Петър_Пеловски_1.rb
+++ class2_homework/Петър_Пеловски_1.rb
@@ -3,11 +3,18 @@ require "csv"
 income = 0.00
 expense = 0.00
 balance = 0.00
+broqch = 0
 CSV.foreach("text_file.csv") do |row|
-	if row[0] >= ARGV[0] && row[0] <= ARGV[1]
+	if row[0] == ARGV[0]
+		broqch = 1
+	end
+	if broqch == 1
 		income=income+row[1].to_f
 		expense=expense+row[2].to_f
 	end
+	if row[0] == ARGV[1]
+		broqch = 0
+	end
 end
 balance = income - expense
-puts "#{income} #{expense} #{balance}"
+printf "#{income.to_f} #{expense.to_f} #{balance.to_f}"
