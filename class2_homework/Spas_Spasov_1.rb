4efc3485fe8728876fea830566d755fc379af434
Spas Spasov <s.spasov96@abv.bg>
2013-09-28 13:54:32 +0300

    Create Spas_Spasov_1.rb

---
+++ class2_homework/Spas_Spasov_1.rb
@@ -0,0 +1,14 @@
+require 'csv'
+require 'date'
+nachalo = Date.parse(ARGV[0])
+krai = Date.parse(ARGV[1])
+dohod = 0.00
+razhod = 0.00
+CSV.foreach("bank.csv") do |row|
+	data = Date.parse(row[0])
+	if data >= nachalo and d <= krai
+		dohod = dohod + row[1].to_f
+		razhod = razhod + row[2].to_f
+	end
+end
+printf "%.2f,%.2f,%.2f", dohod, razhod, dohod - razhod
