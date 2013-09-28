1ca5f65bcccd58de3e1fc425c14fd6de631030ba
Spas Spasov <s.spasov96@abv.bg>
2013-09-28 21:45:52 +0300

    Update Spas_Spasov_1.rb

--- class2_homework/Spas_Spasov_1.rb
+++ class2_homework/Spas_Spasov_1.rb
@@ -1,14 +1,18 @@
-require 'csv'
-require 'date'
-nachalo = Date.parse(ARGV[0])
-krai = Date.parse(ARGV[1])
-dohod = 0.00
-razhod = 0.00
+require "csv"
+require "date"
+
+first_date = Date.parse(ARGV[0])
+second_date = Date.parse(ARGV[1])
+
+income = 0.00
+expense = 0.00
+
 CSV.foreach("bank.csv") do |row|
-	data = Date.parse(row[0])
-	if data >= nachalo && d <= krai
-		dohod = dohod + row[1].to_f
-		razhod = razhod + row[2].to_f
+	file_date = Date.parse(row[0])
+	if file_date >= first_date && file_date <= second_date
+		income=income+row[1].to_f
+		expense=expense+row[2].to_f
 	end
 end
-printf "%0.2f,%.2f,%.2f", dohod, razhod, dohod - razhod
+
+printf("%0.2f,%0.2f,%0.2f",income,expense,income - expense)
