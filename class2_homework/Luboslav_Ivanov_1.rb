cb22571d480220333b25853e77dd196285c608fd
Luboslav_Ivanov_tues <killer18ys@gmail.com>
2013-09-30 00:05:21 +0300

    Create Luboslav_Ivanov_1.rb

---
+++ class2_homework/Luboslav_Ivanov_1.rb
@@ -0,0 +1,15 @@
+require "csv"
+
+income = 0.00
+expense = 0.00
+date1 = ARGV[0]
+date2 = ARGV[1]
+
+CSV.foreach("bank.csv") do |row|
+	if (date1 <= row[0]) && (row[0] <= date2)
+		income=income+row[1].to_f
+		expense=expense+row[2].to_f
+	end
+end
+
+puts "#{income},#{expense},#{income - expense}"
