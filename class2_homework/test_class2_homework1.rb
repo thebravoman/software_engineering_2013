60218cf0ef32bced7e08b49bbd7d17a9d54cfff0
Kiko <k.mitov@robopartans.com>
2013-09-29 09:53:29 +0300

    Adding exception handling for the tests

--- class2_homework/test_class2_homework1.rb
+++ class2_homework/test_class2_homework1.rb
@@ -2,9 +2,14 @@ require 'csv'
 
 CSV.open("results1.csv", "w") do |csv|
 	Dir.glob("*_1.rb") do |file|
-		result = `ruby #{file} 20/09/13 30/09/13`
-		result = result.gsub(/[\n\r]/,"")
-		csv << [file, result, result == ARGV[0]]
+		begin
+			result = `ruby #{file} 20/09/13 30/09/13`
+			result = result.gsub(/[\n\r]/,"")
+			csv << [file, result, result == ARGV[0]]
+		rescue
+			result = "Exception for #{file}"
+			csv << [file, result, false]
+		end
 	end
 end
 
--- class2_homework/test_class2_homework3.rb
+++ class2_homework/test_class2_homework3.rb
@@ -2,8 +2,12 @@ require 'csv'
 
 CSV.open("results3.csv", "w") do |csv|
 	Dir.glob("*_3.rb") do |file|
-		result = `ruby #{file}`
-		result = result.gsub(/[\n\r]/,"")
+		begin 
+			result = `ruby #{file}`
+			result = result.gsub(/[\n\r]/,"")
+		rescue
+			result = "Exception for #{file}"
+		end
 		csv << [file, result, result == ARGV[0]]
 	end
 end
