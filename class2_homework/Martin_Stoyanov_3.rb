dbcba0c757a8fb39d38272bda2177426517a7aad
Defacer <defacer19@gmail.com>
2013-09-29 20:32:10 +0300

    Task3

---
+++ class2_homework/Martin_Stoyanov_3.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+income = 0.00
+highest = 0.00
+date = ''
+
+CSV.foreach('bank.csv') do |row|
+	income = row[1].to_f - row[2].to_f
+	if income > highest
+		highest = income
+		date = row[0]
+	end
+end
+puts date
