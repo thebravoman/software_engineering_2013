6ae750e4aa8368a718d7298dfc888b79b7145e8f
apanayotov <asen.panayotov@gmail.com>
2013-09-29 12:40:43 -0700

    Asen Panayotov Homework

---
+++ class2_homework/Asen_Panayotov_1.rb
@@ -0,0 +1,27 @@
+require 'date'
+require 'csv'
+
+
+first_day=Date.strptime(ARGV[0],'%d/%m/%Y' )
+last_day=Date.strptime(ARGV[1], '%d/%m/%Y')
+
+income = 0.00
+expense = 0.00
+bal=0.00
+
+CSV.foreach('bank.csv') do |row|
+		
+		day = Date.strptime(row[0], '%d/%m/%Y')
+
+	if day>=first_day && day <= last_day
+
+		income=income+row[1].to_f
+		expense=expense+row[2].to_f
+
+	end
+end
+
+
+bal= income-expense
+
+printf '%.2f,%.2f,%.2f', income, expense, bal
