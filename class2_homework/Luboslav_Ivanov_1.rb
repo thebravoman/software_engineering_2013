f09a8410680acb14f4a82d16f2f171273c425060
Luboslav_Ivanov_tues <killer18ys@gmail.com>
2013-09-30 15:40:17 +0300

    Update Luboslav_Ivanov_1.rb

--- class2_homework/Luboslav_Ivanov_1.rb
+++ class2_homework/Luboslav_Ivanov_1.rb
@@ -2,14 +2,16 @@ require "csv"
 
 income = 0.00
 expense = 0.00
+total = 0.00
 date1 = ARGV[0]
 date2 = ARGV[1]
 
 CSV.foreach("bank.csv") do |row|
-	if (date1 <= row[0]) && (row[0] <= date2)
-		income=income+row[1].to_f
-		expense=expense+row[2].to_f
-	end
+if (date1 <= row[0]) && (row[0] <= date2)
+	income=income+row[1].to_f
+	expense=expense+row[2].to_f
 end
+end
+total = income - expense
 
-puts "#{income},#{expense},#{income - expense}"
+printf "%.2f,%.2f,%.2f",  income, expense, total	
