006e849695248137b7c8d13124e0667842fb171a
YulianRazkazov <yuli961@abv.bg>
2013-09-30 15:33:21 +0300

    Task 3

---
+++ class2_homework/Yulian_Razkazov_3.rb
@@ -0,0 +1,17 @@
+require 'csv'
+
+c=0.00
+data=""
+CSV.foreach('bank.csv') do |row|
+if data==row[0]
+c+=row[1].to_f
+else
+if row[1].to_f>c
+c=row[1].to_f
+data=row[0]
+end
+end
+end
+puts data
+
+
