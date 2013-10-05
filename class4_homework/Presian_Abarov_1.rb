919adcde75e09e83e72fe9ff91b43c9b0cbbefb9
quasar <quasar@deepspace.(none)>
2013-10-05 15:42:13 +0300

    Presian Abarov

---
+++ class4_homework/Presian_Abarov_1.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+require 'csv'
+
+all = {}
+
+CSV.foreach(ARGV[0] + "eval.csv") do |row|
+	next if row[0].to_s.length != 1
+	all[row[3]] = [row[1] + " " + row[2]]
+end
+
+Dir.glob(ARGV[0] + "results*") do |resultsX_csv|
+	CSV.foreach(resultsX_csv) do |row|
+		names = row[0].split("_")[0..1]
+		name = names[0] + " " + names[1]
+		next unless all.has_key?(name)
+		all[name] << res = row[2] == "true"? 1:0	
+	end
+end
+
+CSV.open("results1.csv","wb") do |csv|
+	all.each do |key, value|
+		csv << value
+	end
+end
