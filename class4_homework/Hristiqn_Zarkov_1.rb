39fd8a7b01555a9bd7a98c8c49b5d1674da2a786
hrisiz <hrisi.zarkov@abv.bg>
2013-10-05 16:23:02 +0300

    Create Hristiqn_Zarkov_1.rb

---
+++ class4_homework/Hristiqn_Zarkov_1.rb
@@ -0,0 +1,40 @@
+require "csv"
+
+names = {}
+CSV.foreach(File.join(ARGV[0],"Evaluation 2013-2014 - Sheet2.csv")) do |row|
+        if (row[0].to_s.length <= 1)
+                if (row[3] == nil)
+                        row[3] = " "
+                end
+                names[row[3]] = row[1].to_s + row[2].to_s
+        end
+end
+first_results = {}
+CSV.foreach("results1.csv") do |row|
+        name = row[0].split("_")[0..1]
+        name = name[0].to_s + " " + name[1].to_s
+        if (row[2] == "true")
+                first_results[name] = "1"
+        else
+                first_results[name] = "0"
+        end
+end
+
+second_results = {}
+CSV.foreach("results1.csv") do |row|
+        name = row[0].split("_")[0..1]
+        name = name[0].to_s + " " + name[1].to_s
+        if (row[2] == "true")
+                second_results[name] = "1"
+        else
+                second_results[name] = "0"
+        end
+end
+
+CSV.open("results1.csv", "w") do |csv|
+        names.sort_by {|a,b| b}.each do |row|
+                if ((first_results[row[0]]) || (second_results[row[0]]))
+                        csv << [row[1],first_results[row[0]],second_results[row[0]]]
+                end
+       end
+end
