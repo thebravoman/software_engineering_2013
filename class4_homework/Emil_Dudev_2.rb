979deee0065ab07bf0d8e61b55d7902366370cc2
Emil Dudev <emildudev@gmail.com>
2013-10-06 16:08:38 +0300

    Homework 4 small fix

--- class4_homework/Emil_Dudev_2.rb
+++ class4_homework/Emil_Dudev_2.rb
@@ -9,23 +9,23 @@ dl = DateTime.parse( '2013-09-30T16:00:00+03:00' )
 d = {}
 CSV.foreach( File.join( ARGV[0], 'names.csv' ) ) do |s|
 	next if s[0].to_s.length != 1
-	s[1] << ' ' << s[2]
+	s[1] = s[1].strip << ' ' << s[2].strip
 	s[3] = s[1] if s[3].nil?
 	d[s[3]] = [s[1], false, false]
 end
 CSV.foreach( File.join( ARGV[0], 'results1.csv' ) ) do |e|
-	n = e[0].split( '_1.rb' )[0].gsub( /_/, ' ' )
+	n = e[0].split( '_1.rb' )[0].tr( '_', ' ' )
 	next unless d.has_key?( n )
 	e[0] = File.join( ARGV[0], e[0] ).to_s
 	d[n][1] = DateTime.strptime( `git log --format="format:%ct" -- #{e[0]}`.lines.first.chomp, '%s' )
 end
 CSV.foreach( File.join( ARGV[0], 'results3.csv' ) ) do |e|
-	n = e[0].split( '_3.rb' )[0].gsub( /_/, ' ' )
+	n = e[0].split( '_3.rb' )[0].tr( '_', ' ' )
 	next unless d.has_key?( n )
 	e[0] = File.join( ARGV[0], e[0] ).to_s
 	d[n][2] = DateTime.strptime( `git log --format="format:%ct" -- #{e[0]}`.lines.first.chomp, '%s' )
 end
-d = d.sort { |a,b| a[0] <=> b[0] }
+d = d.sort { |a,b| a[1][0] <=> b[1][0] }
 d.each do |k,v|
 	t = v[1] && v[2] && v[1] < dl && v[2] < dl
 	v[1] = v[2] if v[2] && ( !v[1] || v[2] > v[1] )
