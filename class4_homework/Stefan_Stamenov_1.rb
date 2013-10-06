79714658d6c560809b558a77a1e87664c56c64e3
Stamenov96 <stefan.stamenov1996@gmail.com>
2013-10-06 18:22:48 +0300

    Task1 Fix

--- class4_homework/Stefan_Stamenov_1.rb
+++ class4_homework/Stefan_Stamenov_1.rb
@@ -6,25 +6,25 @@ CSV.foreach(File.join(ARGV[0],"Evaluation 2013-2014 - Sheet2.csv")) do |row|
 	if row[0].to_s.length!=1
 	  next
 	else 
-	  bgname=row[1].to_s+" "+row[2].to_s
+	  name=row[1].to_s+" "+row[2].to_s
 		next	if row[3].nil?
-	  h[row[3]]=[bgname,0,0]
+	  h[row[3]]=[name,0,0]
 	end
 end
 
 
 CSV.foreach(File.join(ARGV[0],"results1.csv")) do |row|
 	names = row[0].to_s.split("_")[0..1]
-	engname = names[0].to_s + " " + names[1].to_s
-	if h.has_key?(engname)
+	name = names[0].to_s + " " + names[1].to_s
+	if h.has_key?(name)
 	  h[name][1]=row [2]=='true' ? 1 : 0
 	end
 end
 
 CSV.foreach(File.join(ARGV[0],"results3.csv")) do |row|
 	names = row[0].to_s.split("_")[0..1]
-	enname = names[0].to_s + " " + names[1].to_s
-	if h.has_key?(enname)
+	name = names[0].to_s + " " + names[1].to_s
+	if h.has_key?(name)
 	  h[name][2]=row [2]=='true' ? 1 : 0
 	end
 end
