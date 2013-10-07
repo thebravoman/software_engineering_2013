c8c009b675a0e778327cd33681df2c5d878ba873
slaviber <slavimber@gmail.com>
2013-10-07 20:27:40 +0300

    HW

--- class4_homework/Stanislav_Beregov_1.rb
+++ class4_homework/Stanislav_Beregov_1.rb
@@ -1,7 +1,7 @@
 require 'csv'
 # encoding: UTF-8
-names = Hash.new{[0..3]}
-val = [0..3]
+names = Hash.new{["","",""]}
+val = ["","",""]
 
 def parse_name(file_name)
 	names = file_name.split("_")[0..1]
@@ -20,16 +20,19 @@ CSV.foreach("#{ARGV[0]}/results3.csv") do |line|
 	names[name] = val
 end
 CSV.foreach("#{ARGV[0]}/names.csv") do |line|
-	name = line[3]
+	next if line[3] == nil
+	next if line[3].empty?
+	name = line[3].encode('utf-8')
 	if names.has_key?(name)
 		val = names[name]
 		val[2] = line[1] + " " + line[2]
 		names[name] = val
 	end
 end
-#names = names.sort_by { |key,arr| arr[2] }
+names = names.sort_by { |key,arr| arr[2]}
 f = CSV.open( 'results1.csv', 'wb' )
 names.each do |key, value|
+	next if value[2] =~ /^\s*$/
 	f << [value[2]] + [value[0]] + [value[1]]
 end
 f.close
