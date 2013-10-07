1ee815f21cb14ecf18bee91d1d96cabdd0209514
pabarov <pressian.abarov@gmail.com>
2013-10-07 10:05:17 +0300

    update to handle file errors

--- class4_homework/Presian_Abarov_1.rb
+++ class4_homework/Presian_Abarov_1.rb
@@ -7,16 +7,18 @@ all = {}
 CSV.foreach(ARGV[0] + "Evaluation 2013-2014 - Sheet2.csv") do |row|
 	next if row[0].to_s.length != 1
 	next if row[3].nil?
-	all[row[3]] = [row[1] + " " + row[2]]
+	all[row[3]] = [row[1] + " " + row[2], 0, 0]
 end
 
+iter = 1
 Dir.glob(ARGV[0] + "results*") do |resultsX_csv|
 	CSV.foreach(resultsX_csv) do |row|
 		names = row[0].split("_")[0..1]
 		name = names[0] + " " + names[1]
 		next unless all.has_key?(name)
-		all[name] << res = row[2] == "true"? 1:0	
+		all[name][iter] = res = row[2] == "true"? 1:0	
 	end
+	iter += 1
 end
 
 all = all.sort_by { |key, value| key }
