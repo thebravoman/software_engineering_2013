26ffc9c84841318cde32416ba84ee719b1d4c132
georgidimitrov <georgu.dimitrov@gmail.com>
2013-10-09 20:20:24 +0300

    fix2

--- class5_homework/Georgi_Dimitrov_2.rb
+++ class5_homework/Georgi_Dimitrov_2.rb
@@ -1,51 +1,16 @@
-require 'date'
-require 'csv'
-names = Hash.new
-
-CSV.foreach(File.join(ARGV[0], "Evaluation 2013-2014 - Sheet2.csv")) do |row|
-	if row[0].to_s.lenght !=1 and row[0].to_s.lenght !=2
-	bgname = row[1].to_s.strip + '' + row[2].to_s.strip
-	end
-	if row[3].nil?
-		engname = bgname
-	else
-		engname = row[3].to_s
-	end
-	hw1 = "#{engname.tr(' ', '_')}_1.rb"
-	hw3 = "#{engname.tr(' ', '_')}_3.rb"
-	names[engname] = [bgname, hw1, hw3]
-end
-
-date = DateTime.parse('2013-09-30T16:00:00+03:00')
-
-names.each do |x|
-	homework1 = DateTime.parse(`git log --date=iso --format="format:%cd" -- #{x[1]}`).lines.first.chomp
-	homework2 = DateTime.parse(`git log --date=iso --format="format:%cd" -- #{x}[2]`).lines.first.chomp
-	if homework1.nil?
-		x[1] = 0
-	else
-		x[1] = DateTime.parse(homework1.lines.first.chomp)
-	end
-
-	if homework2.nil?
-		x[1] = 0
-	else
-		x[2] = DateTime.parse(homework2.lines.first.chomp)
+hour = 60*60*1000*1000*1000 
+min = 60*1000*1000*1000
+sec = 1000*1000*1000
+millis = 1000*1000
+masiv = []
+File.readlines(ARGV[0]).each do |line|
+	if line =~ /-->/
+		masiv << line
 	end
 end
+first = masiv[0][0..11]
+last = masiv[-1][17..29]
 
-CSV.open("results2.csv", "w") do |csv|
-	names.each do |x,y|
-		if x[1] <= date and x[2] <= date
-			csv << [x[0], x[1,2], "1"]
-		else 
-			csv << [x[0], x[1,2], "0"]
-		end
-	end
-end
-		
-		
-		
-		
-		
-		
+start = ( first[0..1].to_i*hour + first[3..4].to_i*min + first[6..7].to_i*sec + first[9..11].to_i*millis )
+ending= ( last[0..1].to_i*hour + last[3..4].to_i*min + last[6..7].to_i*sec + last[9..11].to_i*millis )
+p ending - start
