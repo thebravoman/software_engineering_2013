c2be990a998ea9d0bd140419f0a44850736738b4
Spas Spasov <s.spasov96@abv.bg>
2013-10-09 14:09:30 +0300

    fix2

--- class5_homework/Spas_Spasov_2.rb
+++ class5_homework/Spas_Spasov_2.rb
@@ -13,7 +13,7 @@ File.readlines(ARGV[0]).each do |l|
 		fl = l[0..11]
 	end
 end
-begin = ( fl[0..1].to_i*h + fl[3..4].to_i*m + fl[6..7].to_i*s + fl[9..11].to_i*ms )
-end = ( ll[0..1].to_i*h + ll[3..4].to_i*min + ll[6..7].to_i*s + ll[9..11].to_i*ms )
-ns = end - begin
+nachalo = ( fl[0..1].to_i*h + fl[3..4].to_i*m + fl[6..7].to_i*s + fl[9..11].to_i*ms )
+krai = ( ll[0..1].to_i*h + ll[3..4].to_i*m + ll[6..7].to_i*s + ll[9..11].to_i*ms )
+ns = krai - nachalo
 p ns
