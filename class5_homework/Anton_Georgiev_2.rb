457a476d33f18282aa3fd7be7707b33f44e1bfa5
anton4o123 <anton4o123@abv.bg>
2013-10-09 17:15:14 +0300

    Task 2 update

--- class5_homework/Anton_Georgiev_2.rb
+++ class5_homework/Anton_Georgiev_2.rb
@@ -5,13 +5,11 @@ j=0
 
 File.open(ARGV[0]).each do |line|
    if line =~ /-->/ && j==0
-     first_time=line.split(" --> ")[0]
-     first_time=Time.parse(first_time)
+     first_time=Time.parse(line.split(" --> ")[0])
      j+=1
    end
    if line =~ /-->/
-     last_time=line.split(" --> ")[1]
-     last_time=Time.parse(last_time)
+     last_time=Time.parse(line.split(" --> ")[1])
    end
 end
 
