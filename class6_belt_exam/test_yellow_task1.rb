c388ac28d26031462c73acc1ffccc1525c3c47e6
Kiko <k.mitov@robopartans.com>
2013-10-10 08:22:42 +0300

    Adding class6 Yellow belt exam tasks

---
+++ class6_belt_exam/test_yellow_task1.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+CSV.open("results1.csv", "w") do |csv|
+	Dir.glob("*_1.rb") do |file|
+		begin
+			`mkdir test_execution`
+			`cp #{file} test_execution`
+			`cd test_execution && ruby #{file} ../bank.csv EUR`
+			result = `diff test_execution/bank_result.csv test_data1/bank_expected.csv`
+			result = result.gsub(/[\n\r]/,"")
+			`rm test_execution/*`
+			csv << [file, result, result == ""]
+		rescue
+			result = "Exception for #{file}"
+			csv << [file, result, false]
+		end
+	end
+end
+
