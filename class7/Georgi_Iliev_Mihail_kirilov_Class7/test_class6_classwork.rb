031ff6a341145415cd2e2ac8ffe0346de3cd7a81
gIliev-120 <g.iliev96@gmail.com>
2013-10-15 09:32:35 +0300

    test

---
+++ class7/Georgi_Iliev_Mihail_kirilov_Class7/test_class6_classwork.rb
@@ -0,0 +1,18 @@
+require 'csv'
+
+CSV.open("results1.csv", "w") do |csv|
+	Dir.glob("*_1.rb") do |file|
+		begin
+			`mkdir test_execution`
+			`cp #{file} test_execution`
+			`cd test_execution && ruby #{file} ../test_data/`
+			result = `diff test_execution/subs.txt test_data/parsed_subs_expected.txt`
+			result = result.gsub(/[\n\r]/,"")
+			`rm test_execution/*`
+			csv << [file,result == ""]
+		rescue
+			result = "Exception for #{file}"
+			csv << [file, result, false]
+		end
+	end
+end
