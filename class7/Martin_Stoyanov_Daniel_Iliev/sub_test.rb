15f1f3960ef87809fccf21f7733f512af34748d9
Defacer <defacer19@gmail.com>
2013-10-15 12:31:14 +0300

    Update sub_test.rb

--- class7/Martin_Stoyanov_Daniel_Iliev/sub_test.rb
+++ class7/Martin_Stoyanov_Daniel_Iliev/sub_test.rb
@@ -3,8 +3,7 @@ require 'csv'
 CSV.open("result.txt", "wb") do |csv|
 Dir.glob("*_1.rb") do |file|
 begin
-    `cd ..`
-    `ruby #{file} subs.txt file.txt`
+    `ruby cd../#{file} subs.txt file.txt`
     result = `diff file.txt subs_expected.txt`  
     csv << [file, result == " "]    #hm
 rescue
