efe637a3d4532b88629c47863b1ec8389ba90f0e
nedelcho-delchev-tues <nedelcho_delchev@abv.bg>
2013-10-15 09:24:38 +0300

    Create test.rb

---
+++ class7/Nedelcho-Delchev-Kiril-Kostadinov/test.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+CSV.open("results.csv","w") do |csv|
+	Dir.glob("*_1.rb") do |file|
+		`ruby#{ARGV[0]}`
+		result= `diff subs.txt subs_expected.txt`
+		result= result.gsub(/[\n\r]/,"")
+		if (results == "")
+			csv << [file, result, true]
+		else
+			csv << [file, result, false]
+		end
+	end
+end
