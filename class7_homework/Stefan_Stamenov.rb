0bde521c0ed7c008bda6546553803e4b8a0be89a
Stamenov96 <stefan.stamenov1996@gmail.com>
2013-10-16 19:44:51 +0300

    HW

--- class7_homework/Stefan_Stamenov.rb
+++ class7_homework/Stefan_Stamenov.rb
@@ -14,20 +14,18 @@ File.open(ARGV[0]).each do |line|
 	b=0; 
 	nline="" 
   elsif b!=1 && line=~ /-->/
-	s1 = line.split(" --> ")[1].split(/:|,/)
+	s1 = line.split(" --> ")[0].split(/:|,/)
     stime= s1[0].to_i*3600000 + s1[1].to_i*60000 + s1[2].to_i*1000 + s1[3].to_i
-	if stime>=ftime+3000
+	if stime<=ftime+3000
 		nline="\n"
-		b=1
 	end
- 
+	b=1
 end
 if (!(line =~ /^[0-9]/) && !(line.empty?)&& !(line=~/-->/))
       line=line.split("\n")[0]
         if line==nil then next
           else   
           arr+=line+nline
-          nline = ""
         end
 end
 
