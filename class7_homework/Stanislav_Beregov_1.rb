546f08e48e3da40095274b9e135fea45e0485f89
slaviber <slavimber@gmail.com>
2013-10-16 17:50:01 +0300

    mega fix

--- class7_homework/Stanislav_Beregov_1.rb
+++ class7_homework/Stanislav_Beregov_1.rb
@@ -6,27 +6,24 @@ newsub = false
 File.readlines(ARGV[0]).each do |line|
         if line =~ /\d{2}:[0-5]\d:[0-5]\d,\d{3} --> \d{2}:[0-5]\d:[0-5]\d,\d{3}/
                 newsub = true
+		s0 = line.split(" --> ")[0].split(/:|,/)
+                s1 = line.split(" --> ")[1].split(/:|,/)
+                tmp = s0[0].to_i*3600000 + s0[1].to_i*60000 + s0[2].to_i*1000 + s0[3].to_i
+                tm2 = s1[0].to_i*3600000 + s1[1].to_i*60000 + s1[2].to_i*1000 + s1[3].to_i
                 if firstdate
                         firstdate = false
-                        s0 = line.split(" --> ")[0].split(/:|,/)
-                        s1 = line.split(" --> ")[1].split(/:|,/)
-                        obj[0] = s0[0].to_i*3600000 + s0[1].to_i*60000 + s0[2].to_i*1000 + s0[3].to_i
-                        obj[1] = s1[0].to_i*3600000 + s1[1].to_i*60000 + s1[2].to_i*1000 + s1[3].to_i
+                        obj[0] = tmp
+                        obj[1] = tm2
                         cattext = false
                 else
-                        s0 = line.split(" --> ")[0].split(/:|,/)
-                        s1 = line.split(" --> ")[1].split(/:|,/)
-                        tmp = s0[0].to_i*3600000 + s0[1].to_i*60000 + s0[2].to_i*1000 + s0[3].to_i
-                        tm2 = s1[0].to_i*3600000 + s1[1].to_i*60000 + s1[2].to_i*1000 + s1[3].to_i
-                        if tm2 <= obj[0]+30000
-
+                        if tmp - obj[1] < 3000
                                 cattext = true
                         else
-                                obj[0] = tmp
-                                obj[1] = tm2
                                 cattext = false
                         end
                 end
+		obj[0] = tmp
+                obj[1] = tm2
                 next                
         end
         if line =~ /^\s*$/
