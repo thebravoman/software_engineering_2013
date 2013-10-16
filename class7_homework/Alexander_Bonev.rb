05c30aad57bbcef5c161c9200d03221278f527d6
Alexander <alexchowe@gmail.com>
2013-10-16 19:42:19 +0300

    Made by Alexander Bonev and Anton Georgiev

---
+++ class7_homework/Alexander_Bonev.rb
@@ -0,0 +1,27 @@
+require 'time'
+subs = String.new
+sub_control=String.new
+second_time=Time.parse("20:20:20")
+j = 0
+i = 0
+
+File.open(ARGV[0]).each do |line|
+        if line =~ /-->/
+                first_time=Time.parse(line.split(" --> ")[0])
+                if second_time - first_time>=3 && j!=0
+                        sub_control="\n"
+                else
+                        sub_control=String.new
+                end
+                second_time=Time.parse(line.split(" --> ")[1])
+                j=1
+                i=0
+        elsif i==1
+                subs += line + sub_control
+        end
+        i+=1
+end
+
+File.open("#{ARGV[0].chomp(".srt")}_result.txt","w") do |file|
+        file.write(subs)
+end
