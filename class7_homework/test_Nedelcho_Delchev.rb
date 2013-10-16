f3126d8e25793d4348e10ee0f0abc4a4fb0481cb
nedelcho-delchev-tues <nedelcho_delchev@abv.bg>
2013-10-16 19:22:04 +0300

    subs

--- class7_homework/test_Nedelcho_Delchev.rb
+++ class7_homework/test_Nedelcho_Delchev.rb
@@ -4,8 +4,8 @@ CSV.open("Nedelcho_Delchev_test_data/result.csv", "w") do |csv|
         Dir.glob("*.rb") do |program|
                 next if program[0..3] == test
                 `mkdir test`
-                `ruby #{program} Nedelcho_Delchev_test_data/28.srt Nedelcho_Delchev_test_data/out.txt`
-                result = `diff Nedelcho_Delchev_test_data/out.txt Nedelcho_Delchev_test_data/Nedelcho_Delchev.txt`
+                `ruby #{program} Nedelcho_Delchev_test_data/subs.srt Nedelcho_Delchev_test_data/subs_expected.txt`
+                result = `diff Nedelcho_Delchev_test_data/subs_expected.txt Nedelcho_Delchev_test_data/subs_expected.txt`
                 result.gsub!(/[\n\r]/, "")
                 if result == ""
                         csv << [file, result, true]
