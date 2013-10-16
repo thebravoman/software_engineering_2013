1c7d4eb54c7cbc0e4d19acbf1003cd110ff0ef45
denislav-7128 <denislav-7128@users.noreply.github.com>
2013-10-16 19:35:58 +0300

    Update

--- class7_homework/Denis_Kalfov_1.rb
+++ class7_homework/Denis_Kalfov_1.rb
@@ -1,21 +1,35 @@
 require 'time'
-sub=String.new
+sub=Array.new(0)
+line1=Array.new(0)
+line2=Array.new(0)
 i=0
 
 File.open(ARGV[0]).each do |line|
 	if line =~ /-->/
 		i=0	
-		line1=Time.parse(line.split("-->")[0])
-		line2=Time.parse(line.split("-->")[1])
+		line1 << Time.parse(line.split("-->")[0])
+		line2 << Time.parse(line.split("-->")[1])
 		
 	elsif  i==1
-		sub=sub+line
+		sub << line
 	end
 
- i=i+1
+i=i+1
 end
 
-File.open("sub.txt","w") do |txt|
-	txt << sub
+i=0
+
+File.open("Denis_Kalfov.txt","w+") do |txt|
+	while i!=line1.length() do
+		
+		if (line1[i+1].to_f)-(line2[i].to_f)>=3
+			txt << sub[i].chomp
+      	    		txt << "\n"
+     	    		i=i+1
+     	        else
+     	        	txt << sub[i].chomp
+     	   		i=i+1
+		end	
+	end
 end
 
