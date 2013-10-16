62a50f0fd55c17d3341baafdc35c6ae4b6acdf0a
d-velev <bamzorlol@gmail.com>
2013-10-16 19:46:51 +0300

    Update Dimitar_Velev_1.rb

--- class7_homework/Dimitar_Velev_1.rb
+++ class7_homework/Dimitar_Velev_1.rb
@@ -1,6 +1,9 @@
 require 'time'
 end_time = nil
 text = String.new()
+text_nl = String.new()
+prov = 0
+diff = 0
 File.open(ARGV[0]).each do |row|
         if row =~ /-->/
                 split = row.split(" --> ")[0..1]
@@ -8,13 +11,17 @@ File.open(ARGV[0]).each do |row|
                 
                 diff = start_time - end_time  if end_time != nil
                 end_time = Time.parse(split[1])        
-        else
-                text << row if row.to_s.length > 5
+                prov = 0
+        elsif prov == 1
+                if diff >= 3
+                         text_nl.delete("\n")
         end
+        text_nl << row
 end
+text.delete("\n")
+text << "\n\n\t" << text_nl
 File.open(ARGV[1], "w") do |row|
         text.each do |line|
-            text.delete("\n")
                 row << line
         end
 end
