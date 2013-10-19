2a062935403d8ba40a53cac71f66dc24b364af52
hrisiz <hrisi.zarkov@abv.bg>
2013-10-19 15:38:29 +0300

    Update Hristiqn_Zarkov_1.rb

--- class8_homework/Hristiqn_Zarkov_1.rb
+++ class8_homework/Hristiqn_Zarkov_1.rb
@@ -2,28 +2,35 @@ require 'csv'
 require 'digest/md5'
 
 array = Array.new
-test_date = "Hristiqn_Zarkov_test_data"
 files_folder = "../class7_homework"
-if (ARGV[0])
-        files_folde = ARGV[0]
+expected_test_file = "Hristiqn_Zarkov_test_data/expected_subs.txt"
+subs_file = "Hristiqn_Zarkov_test_data/subs.srt"
+ARGV.each do |arg|
+        if (arg =~ /.txt/)
+                expected_test_file = arg
+        elsif (arg =~ /.srt/)
+                subs_file = arg
+        else
+                files_folder = arg
+        end
 end
 `mkdir programs`
 files = File.basename(__FILE__)
-if (Digest::MD5.hexdigest(files) == "53415c10bfe45610de863621fde69d38")
+if (Digest::MD5.hexdigest(files) == "53415c10bfe45610de863621fde69d38") #When I wrote this, only God and I understood what I was doing
         Dir.glob(File.join(files_folder,"*.rb"))  do |file|
                 file_content = File.open(file,"r").readlines().to_s
                 if (!(file_content =~ /`/ ) && !(file =~ /test/))
-                        `cp #{file} programs/test.rb`
+                        real_file = file.split("/")[-1]
+                        `cp #{file} programs/#{real_file}`
                         output_file = ""
-                        File.open(File.join("programs","test.rb")).each do |row|
+                        File.open(File.join("programs",real_file)).each do |row|
                                 if (row =~ /"w"/)
                                         row = row.split(",")[0]
                                         row = row.split("(")[1]
                                         output_file = row
                                 end
                         end
-                        name = file.split("/")[-1]
-                        name = name.split("_")[0..1]
+                        name = real_file.split("_")[0..1]
                         if (output_file == "")
                                 is_ok = 0
                                 #array << ["#{name[0]} #{name[1]}","No output File.",is_ok]
@@ -32,23 +39,20 @@ if (Digest::MD5.hexdigest(files) == "53415c10bfe45610de863621fde69d38")
                         elsif (output_file == "ARGV[1]")
                                 output_file = "output.txt"
                         end
-                        `cd programs && ruby test.rb ../#{test_date}/subs.srt output.txt`
+                        `cd programs && ruby #{real_file} ../#{subs_file} output.txt`
                         #if (File.file?(File.join("programs",output_file)))
-                        #        problem = `cd programs && diff ../#{test_date}/expected_subs.txt #{output_file}`
+                        #        problem = `cd programs && diff ../#{expected_test_file} #{output_file}`
                         #else
                         #        array << ["#{name[0]} #{name[1]}","Compilation Fail",0]
                         #        next
                         #end
-                        problem = `cd programs && diff ../#{test_date}/expected_subs.txt #{output_file}`
+                        problem = `cd programs && diff ../#{expected_test_file} #{output_file}`
                         is_ok = 0
                         if (problem == "")
                                 is_ok = 1
                         end
                         array << ["#{name[0]} #{name[1]}",problem,is_ok]
-                p "#{name[0]} #{name[1]}"
-                        
                 end        
-                `rm -f programs/test.rb`
         end
 end
 `rm -f programs -r`
