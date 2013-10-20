db837e4971283a1a5e31ded68496260abccf9e9f
b1o <danail.stoqnov@gmail.com>
2013-10-20 17:37:15 +0300

    Danail_Stoianov_class8_homework_1

--- class8_homework/Danail_Stoqnov_1.rb
+++ class8_homework/Danail_Stoqnov_1.rb
@@ -1,14 +1,12 @@
 require 'csv'
 
-CSV.open("result.csv", "wb") do |csv|
+CSV.open("result1.csv", "wb") do |csv|
 	Dir.glob("*_1.rb") do |file|
 		`copy #{file} Danail_Stoqnov_test_data`
 		Dir.chdir "Danail_Stoqnov_test_data"
     			`ruby #{file} subs.srt #{file.gsub(".rb", ".txt")}` 	
     			result = `diff #{file.gsub(".rb", ".txt")} subs_expected.txt`  
     			result = result.gsub(/[\n\r]/,"")
-    			#csv << [file, result == 1] 
-				#result.gsub(/'	true'/, "1")
 			if result == ""
 				csv << [file, result, 1]
 			else
