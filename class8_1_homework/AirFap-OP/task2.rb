5cc307f6d2dfac5b0dbc574dc8c6c049d84545c4
ivaylohristov96 <ivaylo.hhristov96@gmail.com>
2013-10-23 00:35:33 +0300

    Adding possible solutions

---
+++ class8_1_homework/AirFap-OP/task2.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+info = Hash.new {0.0}
+CSV.foreach(ARGV[0]) do |row|
+	if row[1].to_s.include?("#{ARGV[1].to_s}")
+		info[row[0].gsub("champ_","").gsub("_"," ")] = row[3].to_i
+	end
+end
+info = info.sort_by {|a,b| a}
+CSV.open("champs_result.csv","w") do |csv|
+	info.each do |a,b|
+		csv << [a,b]
+	end
+end
