16110bc4ffc0c5f0db82232968373357ea8422d0
anton4o123 <anton4o123@abv.bg>
2013-10-22 18:03:55 +0300

    Test update

--- class8_1_homework/smartest_team/random_task_test.rb
+++ class8_1_homework/smartest_team/random_task_test.rb
@@ -3,7 +3,8 @@ require 'csv'
 result = 0
 sort_check = 1000
 `ruby #{ARGV[0]} task1.csv 20`
-CSV.foreach("task1_result.csv") do |row|	
+CSV.foreach("task1_result.csv") do |row|
+	if row[1].to_i>0 && row[1].to_i<=2*row[0].to_i	
 	if (row[1].to_f/row[0].to_f).round(10)==(row[2].to_f).round(10)
 		result = 1
 	else
@@ -12,6 +13,10 @@ CSV.foreach("task1_result.csv") do |row|
 	end
 	if sort_check>=row[2].to_f
 		sort_check=row[2].to_f
+	else
+		result = 0
+		break
+	end
 	else
 		result = 0
 		break
