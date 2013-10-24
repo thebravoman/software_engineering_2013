fc3ce6597097601de470f98e440cdd140f840f69
Emil Dudev <emildudev@gmail.com>
2013-10-24 22:07:25 +0300

    LazyGenerators, task 7

---
+++ class8_1_homework/LazyGenerators/test_task_7.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+
+require 'csv'
+
+FILE = 'player_stats.csv'
+
+def do_test( input )
+	`ruby #{input} #{FILE} out.csv`
+	a = CSV.read( 'out.csv' )
+	a.sort_by! { |e| e[5].to_f }
+	return false if a.count != 16
+	return false if ( a.first[5].to_f - a.last[5].to_f ).abs >= 10.0
+
+	h = {}
+	t = CSV.read( FILE )
+	t.each { |e| h[e[0].to_i] = e }
+	t = []
+	
+	a.each do |e|
+		avg = 0.0
+		e[0..4].each { |x| avg += h[x.to_i][1].to_f }
+		avg /= 5.0
+		return false if avg != e[5].to_f
+	end
+	true
+end
+
+puts do_test( ARGV[0] )
