50d49c129f7eb82abbf3bbeb67a99c6154ef9407
Emil Dudev <emildudev@gmail.com>
2013-10-24 22:52:02 +0300

    LazyGenerators, small fix

--- class8_1_homework/LazyGenerators/test_task_7.rb
+++ class8_1_homework/LazyGenerators/test_task_7.rb
@@ -8,6 +8,10 @@ def do_test( input )
 	begin
 		`ruby #{input} #{FILE} out.csv`
 		a = CSV.read( 'out.csv' )
+		used = []
+		a.each { |e| used << e[0..4] }
+		used.flatten!.uniq!
+		return false if used.count != 16*5
 		a.sort_by! { |e| e[5].to_f }
 		return false if a.count != 16
 		return false if ( a.first[5].to_f - a.last[5].to_f ).abs >= 10.0
