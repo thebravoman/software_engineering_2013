39191ac46a097e3515caeeae968fb76858bfd0d5
Emil Dudev <emildudev@gmail.com>
2013-10-24 18:11:30 +0300

    LazyGenerators, small fix

--- class8_1_homework/LazyGenerators/task_4.rb
+++ class8_1_homework/LazyGenerators/task_4.rb
@@ -8,7 +8,9 @@ def do_task( input, output, id )
 	a = CSV.read( input )
 	a.sort_by! { |e| [e[1].to_i, e[0].to_i] }
 	return unless i = a.find_index{ |e| e[0].to_i == id }
-	a = a.slice( ( i-5 )..( i+5 ) )
+	b = i-5
+	b = 0 if b < 0
+	a = a.slice( b..( i+5 ) )
 	CSV.open( output, 'wb' ) do |c|
 		a.each { |e| c << e }
 	end
--- class8_1_homework/LazyGenerators/test_task_4.rb
+++ class8_1_homework/LazyGenerators/test_task_4.rb
@@ -10,7 +10,9 @@ def do_task( input, id )
 	a = CSV.read( input )
 	a.sort_by! { |e| [e[1].to_i, e[0].to_i] }
 	return [] unless i = a.find_index{ |e| e[0].to_i == id }
-	a = a.slice( ( i-5 )..( i+5 ) )
+	b = i-5
+	b = 0 if b < 0
+	a = a.slice( b..( i+5 ) )
 end
 
 def do_test( input, count )
