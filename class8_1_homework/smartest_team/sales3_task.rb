b9036e5a3a1362e6bd5b21e23e59faf82527a4f9
denislav-7128 <denislav-7128@users.noreply.github.com>
2013-10-25 19:34:39 +0300

    sales3

---
+++ class8_1_homework/smartest_team/sales3_task.rb
@@ -0,0 +1,25 @@
+require 'csv'
+card=Hash.new{0}
+splitter=0
+
+CSV.foreach(ARGV[0]) do |row|
+
+	splitter=row[0]
+	splitter_min=splitter.split(":")[1] 
+	splitter=splitter.split(":")[0]
+
+	if ((splitter.to_i<=06) && (splitter_min.to_i<=20))
+	 card[row[2]]+=row[1].to_i
+	else
+		next
+	end
+end
+
+card=card.sort_by{|a,b,c,d|b}
+
+
+CSV.open("sales3_results.csv","w") do |csv|
+		card.each do |a,b|
+  	  csv << [b,a]
+ 		end
+end
