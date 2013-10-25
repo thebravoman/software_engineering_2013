b0cbea011c53ad90437678dfce5fd13e07b86fa2
Alexander Monov <alekmonov@abv.bg>
2013-10-25 18:52:29 +0300

    Modify car tasks 1 and 2

--- class8_1_homework/Logicless/cars_task_2.rb
+++ class8_1_homework/Logicless/cars_task_2.rb
@@ -1,3 +1,5 @@
+#/usr/bin/ruby
+
 require 'csv'
 
  old_cars = 0
@@ -6,12 +8,14 @@ require 'csv'
 CSV.foreach("cars.csv", "r") do |row|
 c = row[3] 
 v = ARGV[0]
-  if (c < v)
-  old_cars += 1
-  end
-  if (c >= v)
-  new_cars += 1  
-  end
+
+if c < v
+    old_cars += 1
+end
+if c >= v
+    new_cars += 1  
+end
+
 info = File.open("Info.txt", "w")
 info.puts "Koli prozvedeni predi ",v," godina ", old_cars,"Koli proizvedeni sled ",v," godina vkl", new_cars
 info.close
