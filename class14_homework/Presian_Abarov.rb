e870a3c02b1af7bfdf10a483f3d7eb21b1ac9346
Presian Abarov <quasar@deep.space>
2013-11-18 19:37:37 +0200

    label

--- class14_homework/Presian_Abarov.rb
+++ class14_homework/Presian_Abarov.rb
@@ -8,13 +8,13 @@ chart_drawer = ChartDrawer.new d
 array_of_values = CSV.read('../class14/data.csv')
 
 max_height = array_of_values[1][1..-1].sort[-1].to_i
-p max_height
 
 (0..max_height).step(max_height/5) do |y|
 	d.text y.to_s, -40, y+4
 	d.line -40, y, 80, y
 end
 
+d.text array_of_values[1][0], array_of_values[1][1..-1].length, -16
 chart_drawer.columns_chart array_of_values
-
+	
 d.finish
