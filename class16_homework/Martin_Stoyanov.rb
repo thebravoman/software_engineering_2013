b0d769929eb1fc3e1ed53b2ceb2c66d7a2a92179
Martin <martin@ubuntu.ubuntu-domain>
2013-11-27 19:47:35 +0200

    ;_;

---
+++ class16_homework/Martin_Stoyanov.rb
@@ -0,0 +1,19 @@
+require_relative 'Martin_Stoyanov/graph_drawer'
+require_relative 'Martin_Stoynaov/vertex'
+require_relative 'Martin_Stoyanov/drawer'
+
+d = Drawer.new "Martin_Stoyanov.svg", 300,350
+graph = GraphDrawer.new d
+d.start
+
+8.times do
+	graph.vs << Vertex.new("#{rand(255)}.#{rand(255)}.#{rand(255)}.#{rand(255)}")
+end
+n=0
+graph.vs.each_index do |n|
+	graph.connections << [graph.vs[n],graph.vs[n+1]]
+	n+=1
+	break if n==8
+end
+graph.draw
+d.finish
