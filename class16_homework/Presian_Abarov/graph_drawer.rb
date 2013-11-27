40620a5f522766b1dc03ef6bda2db3a5eb308231
Presian Abarov <quasar@deep.space>
2013-11-27 19:57:10 +0200

    fix

--- class16_homework/Presian_Abarov/graph_drawer.rb
+++ class16_homework/Presian_Abarov/graph_drawer.rb
@@ -51,3 +51,38 @@ class GraphDrawer
 	end
 	
 end
+
+graph = GraphDrawer.new d
+v1 = Vertex.new("172.16.18.13")
+v2 = Vertex.new("172.16.18.11")
+v3 = Vertex.new("172.16.18.54")
+v4 = Vertex.new("172.16.18.44")
+v5 = Vertex.new("172.16.18.43")
+v6 = Vertex.new("172.16.18.88")
+v7 = Vertex.new("172.16.18.83")
+v8 = Vertex.new("172.16.18.255")
+v9 = Vertex.new("172.16.18.1")
+v10 = Vertex.new("172.16.18.60")
+graph.vs << v1
+graph.vs << v2
+graph.vs << v3
+graph.vs << v4
+graph.vs << v5
+graph.vs << v6
+graph.vs << v7
+graph.vs << v8
+graph.vs << v9
+graph.vs << v10
+graph.edges << [v1,v2]
+graph.edges << [v2,v3]
+graph.edges << [v3,v4]
+graph.edges << [v4,v5]
+graph.edges << [v5,v6]
+graph.edges << [v6,v7]
+graph.edges << [v7,v8]
+graph.edges << [v8,v9]
+graph.edges << [v9,v10]
+graph.edges << [v10,v1]
+graph.draw
+
+d.finish
