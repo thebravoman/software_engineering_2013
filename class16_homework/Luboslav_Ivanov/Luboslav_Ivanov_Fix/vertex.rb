481558bc56916ee2c53b94b0a7ef9c134dd466df
Luboslav Ivanov <killer18ys@gmail.com>
2013-11-28 01:23:46 +0200

    _Fix e pravilniq nachin

---
+++ class16_homework/Luboslav_Ivanov/Luboslav_Ivanov_Fix/vertex.rb
@@ -0,0 +1,18 @@
+require_relative 'drawer'
+
+class Vertex
+	attr_accessor :name
+	attr_accessor :x
+	attr_accessor :y
+		
+	def initialize name
+		self.name = name
+	end
+	
+	def draw drawer
+		drawer.rect 20,20,self.x,self.y
+		drawer.text name,self.x-35,self.y-20
+	end
+		
+	
+end
