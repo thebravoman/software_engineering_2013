d318a040a165baa9ccb56d2c0f228159f058ea58
lz1irq <ivailo96@gmail.com>
2013-12-01 23:34:04 +0200

    not empty commit message

---
+++ class17_homework/Ivaylo_Markov/graph_drawer.rb
@@ -0,0 +1,56 @@
+require_relative "drawer"
+require_relative "vertex"
+
+class GraphDrawer
+	
+	attr_accessor :vs
+	attr_accessor :edges
+	
+	def initialize drawer
+		@drawer = drawer
+		self.vs = []
+		self.edges = []
+	end
+	
+	def draw
+		calculate_positions
+		draw_vertexes
+		draw_edges
+	end
+	
+	def add_vertex name
+			el = Vertex.new(name)
+			self.vs << el
+	end	
+	
+	def add_edge source,target
+		edges << [vs[source], vs[target]]
+	end
+	
+	private
+	def calculate_positions
+		step = (Math::PI*2.0)/vs.size
+		curr = 0
+		vs.each_index do |n|
+			vs[n].x = Math.cos(curr)*200
+			vs[n].y = Math.sin(curr)*200
+			curr += step
+		end
+	end
+	
+	def draw_edges
+		edges.each do |vertexes|
+			x1 = vertexes[0].x
+			y1 = vertexes[0].y
+			x2 = vertexes[1].x
+			y2 = vertexes[1].y
+			@drawer.line x1,y1,x2,y2
+		end
+	end
+
+	def draw_vertexes
+		vs.each { |v|	v.draw @drawer }
+	end
+	
+end
+
