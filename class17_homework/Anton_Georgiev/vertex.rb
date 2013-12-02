fbe7942ed76432043959fc232a70a173d7813d98
anton4o123 <anton4o123@abv.bg>
2013-12-02 17:44:46 +0200

    HW17

---
+++ class17_homework/Anton_Georgiev/vertex.rb
@@ -0,0 +1,14 @@
+class Vertex
+	attr_accessor :name
+	attr_accessor :x
+	attr_accessor :y
+	
+	def initialize name
+		self.name = name
+	end
+	
+	def draw d
+		d.circle self.x,self.y,5
+		d.text self.name,self.x+10,self.y+10
+	end
+end
