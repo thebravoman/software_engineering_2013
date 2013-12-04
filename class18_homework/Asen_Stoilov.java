3e17a3911ed221d1f21942d53015a717bf71be42
asen <asen@asen-EP43-UD3L.(none)>
2013-12-04 21:10:31 +0200

    hw

---
+++ class18_homework/Asen_Stoilov.java
@@ -0,0 +1,25 @@
+public class Asen_Stoilov {
+
+	public static void main(String[] args){
+		String check;
+		if (args.length > 0){
+			if(args[args.length-1].equals("up")){
+				for (int i=0;i<(args.length-1);++i){
+				check = System.getProperty(args[i]);
+					if (check != null){
+						System.out.println(check);
+					}
+				}
+			} else {
+				for (int i=(args.length-2);i>0;--i){
+					check = System.getProperty(args[i]);
+					if (check != null){
+						System.out.println(check);
+					}
+				}
+			} 		
+		}
+	
+	}
+
+}
