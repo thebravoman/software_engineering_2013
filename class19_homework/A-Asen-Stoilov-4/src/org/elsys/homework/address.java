de8622028013fe6b4d6c68bbc8e1564eca6b05d7
asen <asen@asen-EP43-UD3L.(none)>
2013-12-08 13:43:50 +0200

    HW

---
+++ class19_homework/A-Asen-Stoilov-4/src/org/elsys/homework/address.java
@@ -0,0 +1,23 @@
+package org.elsys.homework;
+
+public class address {
+	private String street;
+	private int Postcode;
+	
+	
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public int getPostcode() {
+		return Postcode;
+	}
+	public void setPostcode(int postcode) {
+		Postcode = postcode;
+	}
+	
+	
+	
+}
