f0d290f9422d190412502380ff86a44ab0bbd3ce
slaviber <slavimber@gmail.com>
2013-12-08 17:52:02 +0200

    HW19

---
+++ class19_homework/A-Stanislav_Beregov-28/src/org/elsys/School/Personal_info.java
@@ -0,0 +1,27 @@
+package org.elsys.School;
+
+public class Personal_info {
+	String address;
+	String phone;
+	int UID;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getUID() {
+		return UID;
+	}
+	public void setUID(int uID) {
+		UID = uID;
+	}
+	
+	
+}
