b1eb7d9ed5ddfe068c986609fd9fc3d0e666613c
nedelcho-delchev-tues <nedelcho_delchev@abv.bg>
2013-12-09 18:47:38 +0200

    HomeWork 19

---
+++ class19_homework/B-Nedelcho-Delchev-22/src/Class.java
@@ -0,0 +1,26 @@
+
+public class Class {
+	private int numberOfStudents;
+	private String className;
+	private int averageGrade;
+	
+	
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public int getNumberOfStudents() {
+		return numberOfStudents;
+	}
+	public void setNumberOfStudents(int numberOfStudents) {
+		this.numberOfStudents = numberOfStudents;
+	}
+	public int getAverageGrade() {
+		return averageGrade;
+	}
+	public void setAverageGrade(int averageGrade) {
+		this.averageGrade = averageGrade;
+	}
+}
