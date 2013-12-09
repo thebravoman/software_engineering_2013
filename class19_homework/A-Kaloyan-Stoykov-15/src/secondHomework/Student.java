1d0c8dcf603fed399d04ec814a854bb607f34906
kaloyan Stoykov <zombidust@gmail.com>
2013-12-09 18:39:43 +0200

    Homework-19

---
+++ class19_homework/A-Kaloyan-Stoykov-15/src/secondHomework/Student.java
@@ -0,0 +1,40 @@
+package secondHomework;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private int grade;
+	private int age;
+	private int phoneNumber;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
