8b6a165c1cb6471dfc21614c67574baced412e25
hidr0 <hidr0.frbg@gmail.bg>
2013-12-09 22:07:59 +0200

    Damn Java strikes again

---
+++ class19_homework/B-Mihail-Kirilov-21/src/secondHomework/School.java
@@ -0,0 +1,39 @@
+package secondHomework;
+
+public class School {
+	String principalName;
+	String neighborhood;
+	String email;
+	String name;
+	int number;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getPrincipalName() {
+		return principalName;
+	}
+	public void setPrincipalName(String principalName) {
+		this.principalName = principalName;
+	}
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
