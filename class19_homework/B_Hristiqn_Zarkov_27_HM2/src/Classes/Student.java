d93dfc9cbe43a6b3dee9f648a22b809c9879bc97
hrisiz <hrisi.zarkov@abv.bg>
2013-12-09 12:22:00 +0200

    HM2

---
+++ class19_homework/B_Hristiqn_Zarkov_27_HM2/src/Classes/Student.java
@@ -0,0 +1,36 @@
+package Classes;
+
+public class Student {
+	private String name;
+	private int id;
+	private int age;
+	private String grade;
+	public Student(String name) {
+		setName(name);
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	private void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+}
