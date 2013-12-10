8e6d66f262bf3141b13b5f0b4d6eee91af84331b
alexander <alexander@crunchbang.(none)>
2013-12-10 14:26:24 +0200

    Commiting HW FOR class 19

---
+++ class19_homework/A-Alexander-Bonev-2/Students.java
@@ -0,0 +1,35 @@
+package org.elsys.alexander.bonev;
+
+public class Students {
+
+	private String name;
+	private String spec;
+	private int age;
+	private int grade;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSpec() {
+		return spec;
+	}
+	public void setSpec(String spec) {
+		this.spec = spec;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+}
