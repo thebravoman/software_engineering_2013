2711ff2a752c4e5d4fd11daa170f0d54f45c55c4
Emil Dudev <emildudev@gmail.com>
2013-12-10 22:38:46 +0200

    HW20

---
+++ class20_homework/A-Emil-Dudev-11/src/org/elsys/se/class20_homework/DirectEvaluatorFactory.java
@@ -0,0 +1,20 @@
+package org.elsys.se.class20_homework;
+
+public class DirectEvaluatorFactory implements IEvaluatorFactory {
+
+	public IEvaluator createSumEvaluator() {
+		return new SumEvaluator();
+	}
+
+	public IEvaluator createPowerOnEvaluator() {
+		return this.createPowerOnEvaluator(2.0);
+	}
+
+	public IEvaluator createPowerOnEvaluator(double power) {
+		return new PowerOnEvaluator(power);
+	}
+
+	public IEvaluator createFibonacciEvaluator() {
+		return new FibonacciEvaluator();
+	}
+}
