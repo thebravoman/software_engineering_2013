450ed379b345359fdb4f82eec33f0ba851c8da89
nbudinov <nikolabudinov@mail.bg>
2013-12-11 17:00:39 +0200

    hw19

---
+++ class19_homework/A-Nikola-Budinov-23/src/Student.java
@@ -0,0 +1,34 @@
+
+public class Student {
+
+	private int student_age;
+	private String student_name;
+	private int student_avarage_grade;
+
+	public int getStudent_avarage_grade() {
+		return student_avarage_grade;
+	}
+
+	public void setStudent_avarage_grade(int student_avarage_grade) {
+		this.student_avarage_grade = student_avarage_grade;
+	}
+
+	public String getStudent_name() {
+		return student_name;
+	}
+
+	public void setStudent_name(String student_name) {
+		this.student_name = student_name;
+	}
+
+	public int getStudent_age() {
+		return student_age;
+	}
+
+	public void setStudent_age(int student_age) {
+		this.student_age = student_age;
+	}
+	
+	
+
+}
