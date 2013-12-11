afb02393bb0f455a01270bcabb4452f20492464e
metodin123 <metodin123@abv.bg>
2013-12-11 08:38:12 -0800

    Create School.java

---
+++ class19_homework/A-Metodi-Todorinov-20/School.java
@@ -0,0 +1,34 @@
+public class School {
+
+	
+	
+	public Students[] getStudents() {
+		return students;
+	}
+	public void setStudents(Students[] students) {
+		this.students = students;
+	}
+	public Teacher[] getTeachers() {
+		return teachers;
+	}
+	public void setTeachers(Teacher[] teachers) {
+		this.teachers = teachers;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	private Students[] students = new Students[500];
+	private Teacher[] teachers = new Teacher[50];
+	private String name;
+	private String address;
+	
+}
