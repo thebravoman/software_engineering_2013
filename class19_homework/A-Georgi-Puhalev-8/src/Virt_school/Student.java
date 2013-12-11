71b54ab5aa172cb70672f4ea68f7e532da07db9f
gpuhalev <gesho.puh@gmail.com>
2013-12-11 15:17:19 +0200

    hw

---
+++ class19_homework/A-Georgi-Puhalev-8/src/Virt_school/Student.java
@@ -0,0 +1,16 @@
+package Virt_school;
+
+public class Student extends Person{
+	
+	private int num_in_class;
+	
+	
+	public int getNum_in_class() {
+		return num_in_class;
+	}
+	
+	public void setNum_in_class(int num_in_class) {
+		this.num_in_class = num_in_class;
+	}
+
+}
