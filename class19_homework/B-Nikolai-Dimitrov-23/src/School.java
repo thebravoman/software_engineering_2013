2762749b6e5ac4ee24339137866792fcc910a5ce
NikolaiDimitrov <nickolai_dimitrov@abv.bg>
2013-12-11 19:20:41 +0200

    Homework

---
+++ class19_homework/B-Nikolai-Dimitrov-23/src/School.java
@@ -0,0 +1,26 @@
+public class School {
+		private String schoolName;
+        private String schoolAddress;
+        private int schoolNumber;
+		
+        public String getSchoolName() {
+			return schoolName;
+		}
+		public void setSchoolName(String schoolName) {
+			this.schoolName = schoolName;
+		}
+		public String getSchoolAddress() {
+			return schoolAddress;
+		}
+		public void setSchoolAddress(String schoolAddress) {
+			this.schoolAddress = schoolAddress;
+		}
+		public int getSchoolNumber() {
+			return schoolNumber;
+		}
+		public void setSchoolNumber(int schoolNumber) {
+			this.schoolNumber = schoolNumber;
+		}
+        
+        
+ }
---
+++ class19_homework/B-Nikolai-Dimitrov-23/src/Teacher.java
@@ -0,0 +1,32 @@
+
+public class Teacher {
+		private String teacherName;
+		private String teacherSpec;
+		private int age;
+		private int teacherClass;
+		
+		public String getTeacherName() {
+			return teacherName;
+		}
+		public void setTeacherName(String teacherName) {
+			this.teacherName = teacherName;
+		}
+		public String getTeacherSpec() {
+			return teacherSpec;
+		}
+		public void setTeacherSpec(String teacherSpec) {
+			this.teacherSpec = teacherSpec;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public int getTeacherClass() {
+			return teacherClass;
+		}
+		public void setTeacherClass(int teacherClass) {
+			this.teacherClass = teacherClass;
+		}
+	}
