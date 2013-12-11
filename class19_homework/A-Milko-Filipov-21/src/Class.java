b1623afff6d32fd27dc0bc2ec1ba98146e72f66e
M-Filipov <milko.filipov1@abv.bg>
2013-12-11 16:16:35 +0200

    HW_19

---
+++ class19_homework/A-Milko-Filipov-21/src/Class.java
@@ -0,0 +1,35 @@
+
+public class Class {
+	private int grade;
+	private char par;
+	private int numOfStudents;
+	private String headTeacher;
+	
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public char getPar() {
+		return par;
+	}
+	public void setPar(char par) {
+		this.par = par;
+	}
+	public int getNumOfStudents() {
+		return numOfStudents;
+	}
+	public void setNumOfStudents(int numOfStudents) {
+		this.numOfStudents = numOfStudents;
+	}
+	public String getHeadTeacher() {
+		return headTeacher;
+	}
+	public void setHeadTeacher(String headTeacher) {
+		this.headTeacher = headTeacher;
+	}
+	
+
+
+}
