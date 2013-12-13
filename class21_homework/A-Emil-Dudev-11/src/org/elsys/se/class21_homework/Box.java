f9c72b30687cef21e8c9cbe87e00653642910f44
Emil Dudev <emildudev@gmail.com>
2013-12-14 01:46:45 +0200

    HW21

---
+++ class21_homework/A-Emil-Dudev-11/src/org/elsys/se/class21_homework/Box.java
@@ -0,0 +1,34 @@
+package org.elsys.se.class21_homework;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.LinkedList;
+
+public class Box extends BallContainer {
+	protected final double maxCapacity;
+
+	public Box( double maxCapacity ) {
+		super( );
+		this.maxCapacity = maxCapacity;
+	}
+
+	public boolean add( Ball ball ) {
+		if( this.capacity + ball.capacity > this.maxCapacity )
+			return false;
+
+		return super.add( ball );
+	}
+
+	public List<Ball> getBallsFromSmallest( ) {
+		List<Ball> sorted = new LinkedList<Ball>( this );
+		Collections.sort( sorted, new Comparator<Ball>( ) {
+			@Override
+			public int compare( Ball a, Ball b ) {
+				return Double.compare( a.capacity, b.capacity );
+			}
+		});
+
+		return Collections.unmodifiableList( sorted );
+	}
+}
