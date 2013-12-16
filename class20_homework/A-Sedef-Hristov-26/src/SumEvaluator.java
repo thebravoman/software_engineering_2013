6def184878f0929d4ef33d23f90f56772f32299c
defo1 <ironprogrammer@abv.bg>
2013-12-16 19:33:27 +0200

    Create SumEvaluator.java

---
+++ class20_homework/A-Sedef-Hristov-26/src/SumEvaluator.java
@@ -0,0 +1,15 @@
+package thirdHomework;
+
+public class SumEvaluator implements IEvaluator {
+        private double sum = 0;
+        
+        @Override
+        public void add(double d) {
+                this.sum = this.sum + d;
+        }
+
+        @Override
+        public double evaluate() {
+                return this.sum;
+        }
+}
