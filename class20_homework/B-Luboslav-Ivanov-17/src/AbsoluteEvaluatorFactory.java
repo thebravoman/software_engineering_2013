851b2a0d86bce9e1b1719d10c8836416f0a19a1b
Luboslav Ivanov <killer18ys@gmail.com>
2013-12-17 03:48:00 +0200

    done

---
+++ class20_homework/B-Luboslav-Ivanov-17/src/AbsoluteEvaluatorFactory.java
@@ -0,0 +1,33 @@
+
+public class AbsoluteEvaluatorFactory implements IEvaluatorFactory {
+
+	public IEvaluator createSumEvaluator() {
+		return new SumEvaluator() {
+			public void add(double d) {
+				super.add(Math.abs(d));
+			}
+		};
+	}
+
+	public IEvaluator createPowerOnEvaluator() {
+		return this.createPowerOnEvaluator(2.0);
+	}
+
+
+	public IEvaluator createPowerOnEvaluator(double power) {
+		return new PowerOnEvaluator(power) {
+			public void add(double d) {
+				super.add(Math.abs(d));
+			}
+		};
+	}
+
+	public IEvaluator createFibonacciEvaluator() {
+		return new FibonacciEvaluator() {
+			public void add(double d) {
+				super.add(Math.abs(d));
+			}
+		};
+	}
+
+}
