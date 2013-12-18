a78a9757aa5b97f8766a3f356afe6c4e1a4fef78
Alexandar Apostolov <s.apostolovd@gmail.com>
2013-12-18 19:52:03 +0200

    Create Ball.java

---
+++ class21_homework/B_Alexandar_Apostolov_03/Ball.java
@@ -0,0 +1,14 @@
+package H04;
+
+public class Ball {
+	private int capacity;
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+}
