2904ad10b5d7b867f35de506718f51056fe5fa11
nbudinov <nikolabudinov@mail.bg>
2013-12-18 18:13:04 +0200

    b&b

---
+++ class21_homework/A-Nikola-Budinov-23/src/Ball.java
@@ -0,0 +1,21 @@
+
+public class Ball 
+{
+	protected double capacity;
+	
+	public Ball(int capacity)
+	{
+		this.capacity = capacity;
+	}
+	
+	public double getCapacity()
+	{
+		return capacity;
+	}
+	
+	public void setCapacity(int capacity)
+	{
+		this.capacity = capacity;
+	}	
+
+}
