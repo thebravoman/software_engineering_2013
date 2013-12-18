c7a4d330bd3a023d3a0d4dbbad1fb15fb88b7195
lz1irq <ivailo96@gmail.com>
2013-12-18 20:57:09 +0200

    I AM A DWARF AND I'M DIGGING A HOLE

---
+++ class21_homework/A-Ivaylo-Markov-12/src/org/elsys/se/class21/homework/Ball.java
@@ -0,0 +1,18 @@
+package org.elsys.se.class21.homework;
+
+public class Ball {
+	private double capacity;
+	
+	public Ball(double capacity) {
+		this.capacity = capacity;
+	}
+
+	public double getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(double capacity) {
+		this.capacity = capacity;
+	}
+
+}
