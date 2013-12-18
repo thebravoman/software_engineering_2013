4993a8e2ef63589134a5f6a9b491f6b93e6a91ee
KristianPopov <kristian@popovbg.eu>
2013-12-18 19:42:37 +0200

    Create PowerOnEvaluator.java

---
+++ class20_homework/B-Kristian-Popov-16/src/PowerOnEvaluator.java
@@ -0,0 +1,26 @@
+public class PowerOnEvaluator implements IEvaluator {
+
+        private double currentSum;
+        private double power;
+        
+        public PowerOnEvaluator() {
+                this.currentSum = 0.0;
+                this.power = 2.0;
+        }
+        
+        public PowerOnEvaluator(double p){
+                this.currentSum = 0.0;
+                this.power = p ;
+        }
+        
+        @Override
+        public void add(double d) {
+                this.currentSum += (Math.pow(d, this.power));
+        }
+
+        @Override
+        public Double evaluate() {
+                return this.currentSum;
+        }
+
+}
