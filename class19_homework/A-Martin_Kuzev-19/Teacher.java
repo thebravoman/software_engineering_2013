36f714365bd08b35f1b3256b9f61017a1511b037
m-generala <marti_world@abv.bg>
2014-01-04 14:57:11 +0200

    Create Teacher.java

---
+++ class19_homework/A-Martin_Kuzev-19/Teacher.java
@@ -0,0 +1,26 @@
+public class Teacher {
+
+        private String teacher_name;
+        private String subject;
+        private int teacher_age;
+        
+        public String getTeacher_name() {
+                return teacher_name;
+        }
+        public void setTeacher_name(String teacher_name) {
+                this.teacher_name = teacher_name;
+        }
+        public String getSubject() {
+                return subject;
+        }
+        public void setSubject(String subject) {
+                this.subject = subject;
+        }
+        public int getTeacher_age() {
+                return teacher_age;
+        }
+        public void setTeacher_age(int teacher_age) {
+                this.teacher_age = teacher_age;
+        }
+        
+}
