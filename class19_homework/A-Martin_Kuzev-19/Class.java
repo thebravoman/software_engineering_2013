3c997f386c579be735bb97b354131e63109e052f
m-generala <marti_world@abv.bg>
2014-01-04 14:58:46 +0200

    Create Class.java

---
+++ class19_homework/A-Martin_Kuzev-19/Class.java
@@ -0,0 +1,26 @@
+public class Class {
+        
+        private int grade;
+    private char par;
+    private int studentcount;
+    
+        public int getGrade() {
+                return grade;
+        }
+        public void setGrade(int grade) {
+                this.grade = grade;
+        }
+        public char getPar() {
+                return par;
+        }
+        public void setPar(char par) {
+                this.par = par;
+        }
+        public int getStudentcount() {
+                return studentcount;
+        }
+        public void setStudentcount(int studentcount) {
+                this.studentcount = studentcount;
+        }
+    
+}
