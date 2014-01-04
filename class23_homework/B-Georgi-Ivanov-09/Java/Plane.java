f0236198190c9b2633958a8b39de887fb4b594a8
georgi-ivanov-tues <georgiivanof@abv.bg>
2014-01-04 13:40:34 +0200

    Task completed on Java

--- class23_homework/B-Georgi-Ivanov-09/Java/Plane.java
+++ class23_homework/B-Georgi-Ivanov-09/Java/Plane.java
@@ -5,12 +5,14 @@ public class Plane {
 	private int emptySeasts;
 	private int numOfRows;
 	private int numOfSeatsPerRow;
+	private int afterWhichSeatIsTheWalkway;
 	
-	Plane(int rows, int seatsPerRow){
+	Plane(int rows, int seatsPerRow, int afterWhichSeatIsTheWalkway){
 		seats = new int[rows+1][seatsPerRow+1]; // Just so we start from the first seat of the first row rather than the 0 seat of the 0 row...
 		setNumOfRows(rows);
 		setNumOfSeatsPerRow(seatsPerRow);
 		setEmptySeasts(rows*seatsPerRow);
+		setAfterWhichSeatIsTheWalkway(afterWhichSeatIsTheWalkway);
 		emptyPlane();
 	}		
 	
@@ -46,7 +48,7 @@ public class Plane {
 	public void addOnePerson(Group newGroup){
 		for(int rowCounter = 1; rowCounter <= getNumOfRows(); rowCounter++){
 			for(int seatsCounter = 1; seatsCounter <= getNumOfSeatsPerRow(); seatsCounter++){
-				if(seats[rowCounter][seatsCounter] == 0){
+				if(seats[rowCounter][seatsCounter] == 0){ // Seat is empty
 					seats[rowCounter][seatsCounter] = 1; // 1 because the passenger is alone :\
 					emptySeasts--;
 					System.out.println("Passenger 1 seat is row: " + rowCounter + ", seat: " + seatsCounter);
@@ -60,29 +62,36 @@ public class Plane {
 		for(int rowCounter = 1; rowCounter <= getNumOfRows(); rowCounter++){
 			for(int seatsCounter = 1; seatsCounter <= getNumOfSeatsPerRow()-1; seatsCounter++){
 				if(seats[rowCounter][seatsCounter] == 0 && seats[rowCounter][seatsCounter+1] == 0){ // If this seat and the next one are free
-					seats[rowCounter][seatsCounter] = 2;
-					seats[rowCounter][seatsCounter+1] = 2;
-					emptySeasts -= 2;
-					System.out.println("Passenger 2 seat is row: " + rowCounter + ", seat: " + seatsCounter);
-					System.out.println("Passenger 2's friend's seat is row: " + rowCounter + ", seat: " + (seatsCounter+1));
-					return; // Exiting method
+					if(!(seatsCounter == getAfterWhichSeatIsTheWalkway() && seatsCounter+1 == getAfterWhichSeatIsTheWalkway()+1)){ // If the seats are separated by the walkway
+						seats[rowCounter][seatsCounter] = 2;
+						seats[rowCounter][seatsCounter+1] = 2;
+						emptySeasts -= 2;
+						System.out.println("Passenger 2 seat is row: " + rowCounter + ", seat: " + seatsCounter);
+						System.out.println("Passenger 2's friend's seat is row: " + rowCounter + ", seat: " + (seatsCounter+1));
+						return; // Exiting method
+					}
 				}
 			}
 		}
 	}
 	
+	// Trying to understand this method can cause dizziness... Proceed at your own risk!
 	public void addThreePeople(Group newGroup){
 		for(int rowCounter = 1; rowCounter <= getNumOfRows(); rowCounter++){
 			for(int seatsCounter = 1; seatsCounter <= getNumOfSeatsPerRow()-2; seatsCounter++){
 				if(seats[rowCounter][seatsCounter] == 0 && seats[rowCounter][seatsCounter+1] == 0 && seats[rowCounter][seatsCounter+2] == 0){
-					seats[rowCounter][seatsCounter] = 3;
-					seats[rowCounter][seatsCounter+1] = 3;
-					seats[rowCounter][seatsCounter+2] = 3;
-					emptySeasts -= 3;
-					System.out.println("Passenger 3 seat is row: " + rowCounter + ", seat: " + seatsCounter);
-					System.out.println("Passenger 3's friend's seat is row: " + rowCounter + ", seat: " + (seatsCounter+1));
-					System.out.println("Passenger 3's other friend's seat is row: " + rowCounter + ", seat: " + (seatsCounter+2));
-					return; // Exiting method
+					if(!(seatsCounter == getAfterWhichSeatIsTheWalkway() && seatsCounter+1 == getAfterWhichSeatIsTheWalkway()+1 && seatsCounter+2 == getAfterWhichSeatIsTheWalkway()+2)){ // checks if:  3 | 3 3
+					if(!(seatsCounter+1 == getAfterWhichSeatIsTheWalkway() && seatsCounter+2 == getAfterWhichSeatIsTheWalkway()+1)){ // checks if:  3 3 | 3
+						seats[rowCounter][seatsCounter] = 3;
+						seats[rowCounter][seatsCounter+1] = 3;
+						seats[rowCounter][seatsCounter+2] = 3;
+						emptySeasts -= 3;
+						System.out.println("Passenger 3 seat is row: " + rowCounter + ", seat: " + seatsCounter);
+						System.out.println("Passenger 3's friend's seat is row: " + rowCounter + ", seat: " + (seatsCounter+1));
+						System.out.println("Passenger 3's other friend's seat is row: " + rowCounter + ", seat: " + (seatsCounter+2));
+						return; // Exiting method
+						}
+					}
 				}
 			}
 		}
@@ -96,6 +105,15 @@ public class Plane {
 		}
 	}
 
+	public void printAllSeats(){
+	   for(int rowCounter = 1; rowCounter <= getNumOfRows(); rowCounter++){
+	      for(int seatsCounter = 1; seatsCounter <= getNumOfSeatsPerRow(); seatsCounter++){
+	         System.out.printf("%5d ", seats[rowCounter][seatsCounter]);  
+	      }
+	      System.out.println();
+	   }
+	}
+	
 	// GETTERS AND SETTERS
 	public int getEmptySeasts() {
 		return emptySeasts;
@@ -119,5 +137,13 @@ public class Plane {
 
 	public void setNumOfRows(int numOfRows) {
 		this.numOfRows = numOfRows;
+	}
+
+	public int getAfterWhichSeatIsTheWalkway() {
+		return afterWhichSeatIsTheWalkway;
+	}
+
+	public void setAfterWhichSeatIsTheWalkway(int afterWhichSeatIsTheWalkway) {
+		this.afterWhichSeatIsTheWalkway = afterWhichSeatIsTheWalkway;
 	}		
 }
