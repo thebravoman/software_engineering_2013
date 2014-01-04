0d6766c6d05b3c3e970a7dc17f0899adf5861416
m-generala <marti_world@abv.bg>
2014-01-04 15:15:52 +0200

    Create Box.java

---
+++ class21_homework/A-Martin_Kuzev-19/src/Box.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Box extends BallContainer
+{
+        private double boxCapacity;
+        
+        public Box(double boxCapacity)
+        {
+                this.boxCapacity = boxCapacity;
+        }
+        
+        public void add(Ball b)
+        {
+                if (balls.size() < boxCapacity)
+                {
+                        balls.add(b);
+                        capacity+=b.capacity;
+                }
+
+        }
+        
+        public LinkedList<Ball> getBallsFromSmallest()
+        {
+                LinkedList<Ball> fromSmallest = new LinkedList<Ball>( (Collection<? extends Ball>) this);
+                Collections.sort(fromSmallest, new Comparator<Ball>()
+                {
+                        public int compare(Ball p1, Ball p2)
+                        {
+                                return Double.compare(p1.capacity, p2.capacity);
+                        }
+                });
+                return fromSmallest;
+        }
+        
+        
+        
+
+}
