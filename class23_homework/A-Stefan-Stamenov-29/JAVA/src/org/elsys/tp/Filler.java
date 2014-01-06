a00c747b364da346ff7308dfbe8158dfc5cea7b5
Stamenov96 <stefan.stamenov1996@gmail.com>
2014-01-06 15:47:08 +0200

    Update Filler.java

--- class23_homework/A-Stefan-Stamenov-29/JAVA/src/org/elsys/tp/Filler.java
+++ class23_homework/A-Stefan-Stamenov-29/JAVA/src/org/elsys/tp/Filler.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 
 public class Filler {
 	public void planeFiller() throws IOException {
-		Plane airbusA320 = new Plane();
+		Plane Boeing747 = new Plane();
 		Randomizer generator = new Randomizer();
 		int people = 0, group = 0;
 		System.out.println("The default values of the seats in the plane");
-		airbusA320.print();
+		Boeing747.print();
 		System.out.println("-------------------------------");
 		System.out.println("Please press Enter to continiute ...");
 		while (people < 162) {
@@ -17,10 +17,10 @@ public class Filler {
 
 			System.out.print("number of people which is getting in : ");
 			System.out.println(group);
-			if (airbusA320.collocation(group) == 1) {
+			if (Boeing747.collocation(group) == 1) {
 				people += group;
 			}
-			airbusA320.print();
+			Boeing747.print();
 			System.out.println("------------------------");
 			System.out.print("Number of people in the plane : ");
 			System.out.println(people);
