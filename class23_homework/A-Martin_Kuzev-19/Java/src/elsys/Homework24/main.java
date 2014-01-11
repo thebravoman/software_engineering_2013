bfaa77634cfc5c8873b9b4d3c27e1a37894d2cb6
m-generala <marti_world@abv.bg>
2014-01-11 14:56:31 +0200

    Create main.java

---
+++ class23_homework/A-Martin_Kuzev-19/Java/src/elsys/Homework24/main.java
@@ -0,0 +1,11 @@
+package elsys.Homework24;
+
+public class Main {
+
+        public static void main(String[] args) {
+                Plane passengers = new Plane(27, 6);
+                
+                passengers.prepare();
+                passengers.print();
+        }
+}
