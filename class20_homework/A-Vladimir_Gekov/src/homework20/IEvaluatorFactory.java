d47c93783544113e27c357ef007b16a863da2f0d
vladimirgekov <vladimirgekov7@gmail.com>
2014-01-13 16:40:07 +0200

    Create IEvaluatorFactory.java

---
+++ class20_homework/A-Vladimir_Gekov/src/homework20/IEvaluatorFactory.java
@@ -0,0 +1,12 @@
+package Homework20;
+
+public interface IEvaluatorFactory {
+
+        public IEvaluator createSumEvaluator();
+
+        public IEvaluator createPowerOnEvaluator();
+
+        public IEvaluator createPowerOnEvaluator(double power);
+
+        public IEvaluator createFibonaciEvaluator();
+}
