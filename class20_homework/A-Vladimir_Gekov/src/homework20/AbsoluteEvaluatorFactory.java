14266026c7399bc5102cd49aada974a474fc0a28
vladimirgekov <vladimirgekov7@gmail.com>
2014-01-13 16:37:28 +0200

    Create AbsoluteEvaluatorFactory.java

---
+++ class20_homework/A-Vladimir_Gekov/src/homework20/AbsoluteEvaluatorFactory.java
@@ -0,0 +1,33 @@
+package Homework20;
+
+public class AbsoluteEvaluatorFactory implements IEvaluatorFactory {
+
+        @Override
+        public IEvaluator createSumEvaluator() {
+                return new SumEvaluator() {
+                        public void add(double d) {
+                                super.add(Math.abs(d));
+                        }
+                };
+        }
+
+        @Override
+    public IEvaluator createPowerOnEvaluator() {
+                        return this.createPowerOnEvaluator(2.0);
+    }
+
+        @Override
+        public IEvaluator createPowerOnEvaluator(double power) {
+                return new PowerOnEvaluator(power) {
+            public void add(double d) {
+                    super.add(Math.abs(d));
+            }
+                };
+        }
+
+        @Override
+        public IEvaluator createFibonaciEvaluator() {
+                return null;
+        }
+
+}
