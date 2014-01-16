2df78df3f91680f63166ba6c5466e9dbdb9c0ab0
pabarov <pressian.abarov@gmail.com>
2014-01-16 17:55:15 +0200

    Create PressianAbarov2.java

---
+++ class25_belt_exam/green/A-Pressian-Abarov-25/src/org/elsys/se/beltexam/PressianAbarov2.java
@@ -0,0 +1,24 @@
+package org.elsys.se.beltexam;
+
+public class Chart implements PressianAbarov2 {
+	private int value1;
+	private int value2;
+	private int value3;
+
+
+	Chart(int value1, int value2, int value3){
+				this.value1 = value1;
+				this.value2 = value2;
+				this.value3 = value3;
+	}
+
+	public int getValue1() {
+		return value1;
+	}
+	public int getValue2() {
+		return value2;
+	}
+	public int getValue3() {
+		return value3;
+	}
+}
