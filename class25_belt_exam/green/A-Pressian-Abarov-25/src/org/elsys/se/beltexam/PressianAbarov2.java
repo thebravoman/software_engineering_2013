2af60b38d99ea31929f0aea5e26f43f7756e6945
pabarov <pressian.abarov@gmail.com>
2014-01-16 17:59:16 +0200

    Update PressianAbarov2.java

--- class25_belt_exam/green/A-Pressian-Abarov-25/src/org/elsys/se/beltexam/PressianAbarov2.java
+++ class25_belt_exam/green/A-Pressian-Abarov-25/src/org/elsys/se/beltexam/PressianAbarov2.java
@@ -11,14 +11,4 @@ public class Chart implements PressianAbarov2 {
 		this.value2 = value2;
 		this.value3 = value3;
 	}
-
-	public int getValue1() {
-		return value1;
-	}
-	public int getValue2() {
-		return value2;
-	}
-	public int getValue3() {
-		return value3;
-	}
 }
