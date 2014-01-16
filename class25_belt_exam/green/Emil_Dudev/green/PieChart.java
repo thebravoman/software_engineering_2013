c71317c2bfd96d06e7abf8de28b1ff6124d0ad7b
Live session user <ubuntu@ubuntu.(none)>
2014-01-16 15:52:54 +0000

    Belt

---
+++ class25_belt_exam/green/Emil_Dudev/green/PieChart.java
@@ -0,0 +1,10 @@
+package green;
+
+public class PieChart extends EmilDudev4 {
+	public PieChart( int v1, int v2, int v3 ) {
+		super(v1, v2, v3);
+	}
+	public boolean decrement() {
+		return value2 > 0;
+	}
+}
