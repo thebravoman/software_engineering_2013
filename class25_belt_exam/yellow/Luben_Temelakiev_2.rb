38e19779831d401c4a02cde8a3371bbaf4bbc285
Wofho <ltemelakiev@gmail.com>
2014-01-16 17:20:03 +0200

    yellow_belt

---
+++ class25_belt_exam/yellow/Luben_Temelakiev_2.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+result = Hash.new(0)
+
+CSV.foreach(ARGV[0]) do |row|
+        name = row[0].gsub("champ_","").gsub("_"," ")
+        klas = row[1].split("/")
+        if klas[0] != ARGV[1] || klas[1] != ARGV[1]
+                result[name] = row[3].to_i
+        end
+end
+
+result = result.sort_by{|result|result[1]}.reverse
+
+CSV.open("champs_result.csv", "w") do |csv|
+        result.each do |element|
+                csv << element
+        end
+end
