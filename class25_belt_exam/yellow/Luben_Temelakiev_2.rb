7bd86abdbf30adf837b9b1fd6893eeb007733ba9
Wofho <ltemelakiev@gmail.com>
2014-01-16 17:30:14 +0200

    Update Luben_Temelakiev_2.rb

--- class25_belt_exam/yellow/Luben_Temelakiev_2.rb
+++ class25_belt_exam/yellow/Luben_Temelakiev_2.rb
@@ -5,7 +5,7 @@ result = Hash.new(0)
 CSV.foreach(ARGV[0]) do |row|
         name = row[0].gsub("champ_","").gsub("_"," ")
         klas = row[1].split("/")
-        if klas[0] != ARGV[1] || klas[1] != ARGV[1]
+        if klas[0] != ARGV[1] && klas[1] != ARGV[1]
                 result[name] = row[3].to_i
         end
 end
