c7f7fe7eeb7f64ab323e91bf7cb2468c8db8423f
gpuhalev <gesho.puh@gmail.com>
2014-01-16 18:12:23 +0200

    main

--- class25_belt_exam/green/A-Georgi_Puhalev-8/src/Main.java
+++ class25_belt_exam/green/A-Georgi_Puhalev-8/src/Main.java
@@ -1,26 +1,9 @@
-import java.io.IOException;
-
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		int i = 0;
-		String delims = "[, ]+";
+		
 		while(true){
-			String line = String.valueOf(System.in.read());
-			String[] tokens = line.split(delims);
-			if(args.length == 3){
-				i++;
-				
-				if (i%2==0){
-					PieChart chart = new PieChart();
-				}else{
-					BarChart chart = new BarChart();
-				}
-			}else if(args.length==1){
-				break;
-			}else{
-				System.out.println("Invalid arguments!");
-			}
+			Parser parser = new Parser();
 		}
 	}
 }
