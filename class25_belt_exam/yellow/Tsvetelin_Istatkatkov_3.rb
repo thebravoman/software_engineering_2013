c613fa131b3833e716940ee81fb3368523f18546
Tsvetelin-I <sog_ceci@abv.bg>
2014-01-16 17:24:16 +0200

    Create Tsvetelin_Istatkatkov_3.rb

---
+++ class25_belt_exam/yellow/Tsvetelin_Istatkatkov_3.rb
@@ -0,0 +1,20 @@
+require 'csv'
+
+girls=Array.new()
+upyears = ARGV[1].to_i + 2
+downyears = ARGV[1].to_i - 2
+CSV.foreach(ARGV[0]) do |row|
+	if row[3] == "Sama" && upyears > row[1].to_i && downyears < row[1].to_i
+		girls << row
+	end
+end
+p girls
+girls.sort_by! {|a| a[1].to_i}
+p girls
+CSV.open("momicheta_result.csv","w") do |csv|
+	girls.each do |f|
+		csv << f
+	end
+end
+	
+	
