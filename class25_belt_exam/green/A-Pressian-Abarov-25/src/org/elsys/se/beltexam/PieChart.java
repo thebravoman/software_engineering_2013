c914e4198eacfd2d678e98300828b52520df4643
pabarov <pressian.abarov@gmail.com>
2014-01-18 17:53:40 +0200

    Update PieChart.java

--- class25_belt_exam/green/A-Pressian-Abarov-25/src/org/elsys/se/beltexam/PieChart.java
+++ class25_belt_exam/green/A-Pressian-Abarov-25/src/org/elsys/se/beltexam/PieChart.java
@@ -1,12 +1,12 @@
 package org.elsys.se.beltexam;
 
-public class PieChart extends Chart implements PressianAbarov2{
-	
-	PieChart(int value1, value2, value3) {
-		super(value1, value2, value3);
+public class PieChart extends Chart implements PressianAbarov2 {
+
+	PieChart(int value1, int value2, int value3) {
+			super(value1, value2, value3);
 	}
-	
+
 	public void presian() {
-		this.value2 -= value1;
+		this.setValue2(this.getValue2() - this.getValue1());
 	}
 }
