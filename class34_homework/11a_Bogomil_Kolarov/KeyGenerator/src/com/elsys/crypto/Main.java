8c249af607a312d6231ac8deddf3743a156d22f5
Bogi Kolarov <kbogomil@gmail.com>
2014-04-08 01:33:02 +0300

    First task done

---
+++ class34_homework/11a_Bogomil_Kolarov/KeyGenerator/src/com/elsys/crypto/Main.java
@@ -0,0 +1,21 @@
+package com.elsys.crypto;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int[] publicKey = new int[2];
+		int[] privateKey = new int[2];
+		
+		Encryption enc = new Encryption(128, 512);
+		
+		publicKey[0] = enc.getN();
+		publicKey[1] = enc.getE();
+		privateKey[0] = enc.getN();
+		privateKey[1] = enc.getD();
+		
+		System.out.println("Public Key = " + publicKey[0] + ", " + publicKey[1]);
+		System.out.println("Private Key = " + privateKey[0] + ", " + privateKey[1]);
+	}
+	
+}
