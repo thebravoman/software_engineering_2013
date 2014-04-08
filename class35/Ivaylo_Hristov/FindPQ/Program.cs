53e372cff9934a48b5cafbe1da57ea7f18fe776b
Ivaylo Hristov <sshotbender@gmail.com>
2014-04-08 16:59:59 +0300

    Find PQ

---
+++ class35/Ivaylo_Hristov/FindPQ/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace FindPQ
+{
+    using System;
+
+    public class Program
+    {
+        public static bool IsPrime(int number)
+        {
+            double boundary = Math.Floor(Math.Sqrt(number));
+
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            for (int i = 2; i <= boundary; ++i)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        public static void Main()
+        {
+            int n;
+            int[] pq;
+
+            Console.Write("Enter n: ");
+            n = int.Parse(Console.ReadLine());
+
+            pq = FindPQ(n, 128, 1024);
+            Console.WriteLine(pq[0] + " " + pq[1]);
+        }
+
+        public static int[] FindPQ(int n, int min, int max)
+        {
+            int i = max;
+
+            for (; i > min; i--)
+                if (IsPrime(i))
+                    if (IsPrime(n / i)) break;
+
+            return new int[] { i, n / i };
+        }
+    }
+}
