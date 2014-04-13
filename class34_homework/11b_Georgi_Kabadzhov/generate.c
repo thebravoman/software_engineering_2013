5043b501729feb9af1c21fefc4325cfa0e12e5f4
RoshHoul <leprik@abv.bg>
2014-04-13 18:23:27 +0300

    Create generate.c
    tcu tcu

---
+++ class34_homework/11b_Georgi_Kabadzhov/generate.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int gcd (int x, int y) {
+  int m,i;
+  if(x>y)
+       m=y;
+  else
+       m=x;
+
+  for(i=m;i>=1;i--){
+    if(x%i==0&&y%i==0){
+       break;
+    }
+  }
+  return i;
+}
+
+int main(){
+  int primes [] = {131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251};
+  srand(time(NULL));
+  int p = rand() % 23;
+  int q = rand() % 23;
+  while(p == q){
+    q = rand() % 23;
+  }
+  p = primes[p];
+  q = primes[q];
+  int n = p*q;
+  int fn = (p-1)*(q-1);
+  int e = (rand() % fn-2) + 2;
+  while (gcd(e, fn) != 1) {
+    e = (rand() % fn-2) + 2;  
+  }
+  
+  int d = 1;
+  while(1){
+    if(d*e % fn == 1) {
+      break;
+    }
+    d++;
+  }
+  printf("public key = %d, %d\n",n, e);
+  printf("private key = %d, %d\n",n, d);
+  return 0;
+}
