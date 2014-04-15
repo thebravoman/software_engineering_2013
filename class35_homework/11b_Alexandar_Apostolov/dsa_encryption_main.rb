45453b6505536a66493385972d3eafacb2db4e11
Alexandar Apostolov <s.apostolovd@gmail.com>
2014-04-15 12:29:03 +0300

    Update dsa_encryption_main.rb

--- class35_homework/11b_Alexandar_Apostolov/dsa_encryption_main.rb
+++ class35_homework/11b_Alexandar_Apostolov/dsa_encryption_main.rb
@@ -7,6 +7,6 @@ require './dsa_encryption_functionality.rb'
 puts "Enter a message to encrypt:"
 msg = gets.chomp.to_i
 keys = generate_keys
-#enc_msg = encryption(keys, msg)
+enc_msg = encryption(keys, msg)
 #decryption(keys, enc_msg)
 
