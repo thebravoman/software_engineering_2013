7b77531088e9b266977ad10f79cd56daa6d35c5b
Defacer <defacer19@gmail.com>
2014-05-18 14:37:39 +0300

    Update Martin_Stoyanov.rb

--- class36_homework/Martin_Stoyanov.rb
+++ class36_homework/Martin_Stoyanov.rb
@@ -10,14 +10,14 @@ d = gets.chomp.to_i
 
 encryption = String.new
 messsge.split("").each do |char|
-      encryption += (((char.ord**e) % n).to_s).rjust(chunk, '0')
+	encryption += (((char.ord**e) % n).to_s).rjust(chunk, '0')
 end
 
 puts "Encryption: #{encryption}"
 
 decryption = String.new
 encryption.scan(/.{#{chunk}}/).each do |ch|
-	    decryption += ((ch.to_i**d) % n).chr
+	decryption += ((ch.to_i**d) % n).chr
 end
 
 puts "Decryption: #{decryption}"
